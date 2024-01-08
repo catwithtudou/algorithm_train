30a8e51959206297cfd399af6cc11eb54b083e82
catwithtudou <949812478@qq.com>
2024-01-08 22:55:29 +0800

    update: init the 447

---
+++ go/leetcode/447.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func numberOfBoomerangs(points [][]int) (ans int) {
+	for _, p := range points {
+		cnt := map[int]int{}
+		for _, q := range points {
+			dis := (p[0]-q[0])*(p[0]-q[0]) + (p[1]-q[1])*(p[1]-q[1])
+			cnt[dis]++
+		}
+		for _, m := range cnt {
+			ans += m * (m - 1)
+		}
+	}
+	return
+}
