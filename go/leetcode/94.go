a4f247e4a909c4d2625854b5834f54b594a9757b
catwithtudou <949812478@qq.com>
2024-02-10 21:39:20 +0800

    update: done the 94

---
+++ go/leetcode/94.go
@@ -0,0 +1,23 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) (res []int) {
+	var inorder func(node *TreeNode)
+	inorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		inorder(node.Left)
+		res = append(res, node.Val)
+		inorder(node.Right)
+	}
+	inorder(root)
+	return
+}
