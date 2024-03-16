6235b9527e6d738c5546c594a2a65a9797e97179
catwithtudou <949812478@qq.com>
2024-03-16 20:31:39 +0800

    update: done the 2684

---
+++ go/leetcode/2684.go
@@ -0,0 +1,23 @@
+package leetcode
+
+func maxMoves(grid [][]int) (ans int) {
+	m, n := len(grid), len(grid[0])
+	var dfs func(int, int)
+	dfs = func(i, j int) {
+		ans = max(ans, j)
+		if ans == n-1 {
+			return
+		}
+		// 向右上/右/右下走一步
+		for k := max(i-1, 0); k < min(i+2, m); k++ {
+			if grid[k][j+1] > grid[i][j] {
+				dfs(k, j+1)
+			}
+		}
+		grid[i][j] = 0
+	}
+	for i := range grid {
+		dfs(i, 0) // 从第一列的任一单元格出发
+	}
+	return
+}
