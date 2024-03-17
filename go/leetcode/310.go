c8fae2ec7c87becdc10fa068410c906fa259c15d
catwithtudou <949812478@qq.com>
2024-03-17 17:23:15 +0800

    update: done the 310 other way

--- go/leetcode/310.go
+++ go/leetcode/310.go
@@ -47,3 +47,43 @@ func findMinHeightTrees(n int, edges [][]int) []int {
 
 	return []int{path[m/2]}
 }
+
+func findMinHeightTrees310(n int, edges [][]int) []int {
+	if n <= 1 {
+		return []int{0}
+	}
+
+	g := make([][]int, n)
+	deg := make([]int, n)
+	for _, e := range edges {
+		x, y := e[0], e[1]
+		g[x] = append(g[x], y)
+		g[y] = append(g[y], x)
+		deg[x]++
+		deg[y]++
+	}
+
+	var q []int
+	for i, d := range deg {
+		if d == 1 {
+			q = append(q, i)
+		}
+	}
+
+	remain := n
+	for remain > 2 {
+		remain -= len(q)
+		tmp := q
+		q = nil
+		for _, x := range tmp {
+			for _, y := range g[x] {
+				deg[y]--
+				if deg[y] == 1 {
+					q = append(q, y)
+				}
+			}
+		}
+	}
+
+	return q
+}
