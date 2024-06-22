c325584b547f8cf227827208cf8f3f2b1990d1b9
catwithtudou <949812478@qq.com>
2024-06-22 17:41:08 +0800

    update: done the 2663

---
+++ go/leetcode/2663.go
@@ -0,0 +1,25 @@
+﻿package leetcode
+
+func smallestBeautifulString(S string, k int) string {
+	limit := 'a' + byte(k)
+	s := []byte(S)
+	n := len(s)
+	i := n - 1
+	s[i]++
+	for i < n {
+		if s[i] == limit {
+			if i == 0 {
+				return ""
+			}
+			s[i] = 'a'
+			i--
+			s[i]++
+		} else if (i > 0 && s[i] == s[i-1]) || (i > 1 && s[i] == s[i-2]) {
+			s[i]++
+		} else {
+			i++
+		}
+	}
+
+	return string(s)
+}
