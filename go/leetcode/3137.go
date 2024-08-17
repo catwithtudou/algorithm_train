78622f16e1eea43d359ab5c8f42d3d0010102561
catwithtudou <949812478@qq.com>
2024-08-17 09:50:46 +0800

    update: done the 3137

--- go/leetcode/3137.go
+++ go/leetcode/3137.go
@@ -1,1 +1,16 @@
-﻿
+﻿package leetcode
+
+func minimumOperationsToMakeKPeriodic(word string, k int) int {
+	n := len(word)
+	cnt := make(map[string]int, 0)
+	for i := k; i <= n; i += k {
+		cnt[word[i-k:i]]++
+	}
+
+	ans := 0
+	for _, v := range cnt {
+		ans = max(ans, v)
+	}
+
+	return n/k - ans
+}
--- rust/src/leetcode/three_thousand_one_hundred_thirty_seven.rs
+++ rust/src/leetcode/three_thousand_one_hundred_thirty_seven.rs
@@ -1,1 +1,18 @@
-﻿
+﻿use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn minimum_operations_to_make_k_periodic(word: String, k: i32) -> i32 {
+        let n = word.len();
+        let k = k as usize;
+        let mut cnt = HashMap::new();
+        for i in (k..=n).step_by(k) {
+            *cnt.entry(&word[i-k..i]).or_insert(0)+=1;
+        }
+
+        let mx = *cnt.values().max().unwrap();
+
+        (n/k) as i32 - mx
+    }
+}
