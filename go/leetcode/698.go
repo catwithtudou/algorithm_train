8a6da14622961813cb25e2d95fae7516926efcfa
catwithtudou <949812478@qq.com>
2024-08-25 10:59:55 +0800

    update: done the 698

---
+++ go/leetcode/698.go
@@ -0,0 +1,41 @@
+﻿package leetcode
+
+import "sort"
+
+func canPartitionKSubsets(nums []int, k int) bool {
+	target := 0
+	for _, v := range nums {
+		target += v
+	}
+	if target%k != 0 {
+		return false
+	}
+	target = target / k
+
+	bucket := make([]int, k)
+	var dfs func(int) bool
+	dfs = func(i int) bool {
+		if i == len(nums) {
+			return true
+		}
+		for j := 0; j < k; j++ {
+			if j > 0 && bucket[j] == bucket[j-1] {
+				continue
+			}
+			if bucket[j]+nums[i] > target {
+				continue
+			}
+			bucket[j] += nums[i]
+			if dfs(i + 1) {
+				return true
+			}
+			bucket[j] -= nums[i]
+
+		}
+		return false
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
+
+	return dfs(0)
+}
