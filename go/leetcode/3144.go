30c2b452cd32023e517922f87fe8c0e6d0ceabb4
catwithtudou <949812478@qq.com>
2024-08-28 11:02:44 +0800

    update: done the 3144

---
+++ go/leetcode/3144.go
@@ -0,0 +1,35 @@
+﻿package leetcode
+
+import "math"
+
+func minimumSubstringsInPartition(s string) int {
+	n := len(s)
+	memo := make([]int, n)
+	var dfs func(int) int
+	dfs = func(i int) int {
+		if i < 0 {
+			return 0
+		}
+		p := &memo[i]
+		if *p > 0 {
+			return *p
+		}
+		res, k, maxCnt := math.MaxInt, 0, 0
+		cnt := [26]int{}
+		for j := i; j >= 0; j-- {
+			c := s[j] - 'a'
+			if cnt[c] == 0 {
+				k++
+			}
+			cnt[c]++
+			maxCnt = max(maxCnt, cnt[c])
+			if i-j+1 == k*maxCnt {
+				res = min(res, dfs(j-1)+1)
+			}
+		}
+		*p = res
+		return res
+	}
+
+	return dfs(n - 1)
+}
---
+++ rust/src/leetcode/three_thousand_one_hundred_forty_four.rs
@@ -0,0 +1,25 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn minimum_substrings_in_partition(s: String) -> i32 {
+        let n = s.len();
+        let mut f = vec![0; n + 1];
+        for i in 0..n {
+            f[i + 1] = i32::MAX;
+            let mut cnt = [0; 26];
+            let (mut k, mut max_cnt) = (0, 0);
+            for j in (0..=i).rev() {
+                let b = (s.as_bytes()[j] - b'a') as usize;
+                if cnt[b] == 0 {
+                    k += 1;
+                }
+                cnt[b] += 1;
+                max_cnt=max_cnt.max(cnt[b]);
+                if (i-j+1) as i32 == k *max_cnt {
+                    f[i+1]=f[i+1].min(f[j]+1);
+                }
+            }
+        }
+        f[n]
+    }
+}
