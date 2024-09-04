2772530345fbedc34fd96b74fbe72d09ffa99bec
catwithtudou <949812478@qq.com>
2024-09-04 08:40:21 +0800

    update: done the 2860

---
+++ go/leetcode/2860.go
@@ -0,0 +1,18 @@
+﻿package leetcode
+
+import "golang.org/x/exp/slices"
+
+func countWays2860(nums []int) int {
+	slices.Sort(nums)
+	ans := 0
+	if nums[0] > 0 {
+		ans++
+	}
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] < i && nums[i] > i {
+			ans++
+		}
+	}
+
+	return ans + 1
+}
---
+++ rust/src/leetcode/two_thousand_eight_hundred_and_sixty.rs
@@ -0,0 +1,14 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn count_ways(mut nums: Vec<i32>) -> i32 {
+        nums.sort_unstable();
+        let mut ans = (nums[0]>0) as i32;
+        for i in 1..nums.len(){
+            if nums[i-1]<i as i32 && nums[i]>i as i32{
+                ans+=1;
+            }
+        }
+        ans+1
+    }
+}
