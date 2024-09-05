c600c9a42a4e6c0168ec6c32a05e1f5c0b63854d
catwithtudou <949812478@qq.com>
2024-09-05 23:33:07 +0800

    update: done the 3174

---
+++ go/leetcode/3174.go
@@ -0,0 +1,16 @@
+﻿package leetcode
+
+import "unicode"
+
+func clearDigits(s string) string {
+	st := []rune{}
+	for _, c := range s {
+		if unicode.IsDigit(c) {
+			st = st[:len(st)-1]
+		} else {
+			st = append(st, c)
+		}
+	}
+
+	return string(st)
+}
---
+++ rust/src/leetcode/three_thousand_one_hundred_seventy_four.rs
@@ -0,0 +1,15 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn clear_digits(s: String) -> String {
+        let mut st = vec![];
+        for c in s.bytes() {
+            if c.is_ascii_digit() {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+        unsafe { String::from_utf8_unchecked(st) }
+    }
+}
