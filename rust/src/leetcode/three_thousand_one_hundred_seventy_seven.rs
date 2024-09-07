8fafe51d87255b66a8d6b40da49cd619f53c95fc
catwithtudou <949812478@qq.com>
2024-09-07 10:18:46 +0800

    update: done the 3177

---
+++ rust/src/leetcode/three_thousand_one_hundred_seventy_seven.rs
@@ -0,0 +1,20 @@
+﻿use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn maximum_length(nums: Vec<i32>, k: i32) -> i32 {
+        let mut dp = HashMap::new();
+        let mut mx = vec![0; (k + 2) as usize];
+
+        for &v in &nums {
+            let mut f: &mut Vec<i32> = dp.entry(v).or_insert(vec![0; (k + 1) as usize]);
+            for j in (0..=k as usize).rev() {
+                f[j] = f[j].max(mx[j]) + 1;
+                mx[j + 1] = mx[j + 1].max(f[j]);
+            }
+        }
+
+        mx[(k + 1) as usize]
+    }
+}
--- rust/src/leetcode/three_thousand_one_hundred_seventy_six.rs
+++ rust/src/leetcode/three_thousand_one_hundred_seventy_six.rs
@@ -7,13 +7,13 @@ impl Solution {
         let mut dp = HashMap::new();
         let mut mx = vec![0; (k + 2) as usize];
         for &v in &nums {
-            let mut f = dp.entry(v).or_insert(vec![0;(k+1)as usize]);
+            let mut f: &mut Vec<i32> = dp.entry(v).or_insert(vec![0;(k+1)as usize]);
             for j in (0..=k as usize).rev(){
                 f[j]=f[j].max(mx[j])+1;
                 mx[j+1]=mx[j+1].max(f[j]);
             }
         }
 
-        mx[(k+2) as usize]
+        mx[(k+1) as usize]
     }
 }
