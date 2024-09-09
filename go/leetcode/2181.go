57f6bed09e2bb270ea0447d2b7e2d4dca29bbc78
catwithtudou <949812478@qq.com>
2024-09-09 22:10:18 +0800

    update: done the 2181

---
+++ go/leetcode/2181.go
@@ -0,0 +1,24 @@
+﻿package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeNodes(head *ListNode) *ListNode {
+	tail := head
+
+	for cur := head.Next; cur.Next != nil; cur = cur.Next {
+		if cur.Val != 0 {
+			tail.Val += cur.Val
+		} else {
+			tail = tail.Next
+			tail.Val = 0
+		}
+	}
+	tail.Next = nil
+
+	return head
+}
