021925bf277918d1556f30d3133bb014438f0ec8
catwithtudou <949812478@qq.com>
2024-09-13 09:39:06 +0800

    update: done the 2398

---
+++ go/leetcode/2398.go
@@ -0,0 +1,24 @@
+﻿package leetcode
+
+func maximumRobots(chargeTimes []int, runningCosts []int, budget int64) int {
+	q := []int{0}
+	left, sum, ans := 0, int64(0), 0
+	for i, x := range chargeTimes {
+		for len(q) > 0 && x >= chargeTimes[q[len(q)-1]] {
+			q = q[:len(q)-1]
+		}
+		q = append(q, i)
+		sum += int64(runningCosts[i])
+
+		for len(q) > 0 && int64(chargeTimes[q[0]])+sum*int64(i-left+1) > budget {
+			if left == q[0] {
+				q = q[1:]
+			}
+			sum -= int64(runningCosts[left])
+			left++
+		}
+		ans = max(ans, i-left+1)
+	}
+
+	return ans
+}
---
+++ rust/src/leetcode/two_thousand_three_hundred_ninty_eight.rs
@@ -0,0 +1,30 @@
+﻿use std::collections::VecDeque;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn maximum_robots(charge_times: Vec<i32>, running_costs: Vec<i32>, budget: i64) -> i32 {
+        let (mut ans,mut left,mut sum) = (0,0,0i64);
+        let mut q = VecDeque::new();
+        for right in 0..charge_times.len(){
+
+            while !q.is_empty() && charge_times[right] >= charge_times[*q.back().unwrap()] {
+                q.pop_back();
+            }
+
+            q.push_back(right);
+            sum+=running_costs[right] as i64;
+
+            while !q.is_empty() && charge_times[*q.front().unwrap()] as i64 + sum * (right-left+1)as i64 > budget {
+                if left == *q.front().unwrap() {
+                    q.pop_front();
+                }
+                sum-=running_costs[left] as i64;
+                left+=1;
+            }
+
+            ans=ans.max(right-left+1);
+        }
+        ans as _
+    }
+}
