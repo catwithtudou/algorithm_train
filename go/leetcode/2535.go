d8d00e53b65566a38011f18c13b46ee666214f45
catwithtudou <949812478@qq.com>
2024-09-26 23:35:38 +0800

    update: done the 2535

---
+++ go/leetcode/2535.go
@@ -0,0 +1,12 @@
+﻿package leetcode
+
+func differenceOfSum(nums []int) (ans int) {
+	for _, x := range nums {
+		ans += x
+		for x > 0 {
+			ans -= x % 10
+			x /= 10
+		}
+	}
+	return ans
+}
---
+++ rust/src/leetcode/two_thousand_five_hundred_thirty_five.rs
@@ -0,0 +1,15 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn difference_of_sum(nums: Vec<i32>) -> i32 {
+        let mut ans = 0;
+        for mut x in nums {
+            ans += x;
+            while x > 0 {
+                ans -= x % 10;
+                x /= 10;
+            }
+        }
+        ans
+    }
+}
