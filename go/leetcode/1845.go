9b41e16462e62f112f6db97d11a3cfb898e3ab3d
catwithtudou <949812478@qq.com>
2024-09-30 18:38:22 +0800

    update: done the 1845

---
+++ go/leetcode/1845.go
@@ -0,0 +1,45 @@
+﻿package leetcode
+
+import (
+	"container/heap"
+	"sort"
+)
+
+type SeatManager struct {
+	sort.IntSlice
+	meats int
+}
+
+func ConstructorSeatManager(n int) SeatManager {
+	return SeatManager{}
+}
+
+func (this *SeatManager) Reserve() int {
+	if len(this.IntSlice) > 0 {
+		return heap.Pop(this).(int)
+	}
+	this.meats += 1
+	return this.meats
+}
+
+func (this *SeatManager) Unreserve(seatNumber int) {
+	heap.Push(this, seatNumber)
+}
+
+func (this *SeatManager) Push(v any) {
+	this.IntSlice = append(this.IntSlice, v.(int))
+}
+
+func (this *SeatManager) Pop() any {
+	a := this.IntSlice
+	v := a[len(a)-1]
+	this.IntSlice = a[:len(a)-1]
+	return v
+}
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * obj := Constructor(n);
+ * param_1 := obj.Reserve();
+ * obj.Unreserve(seatNumber);
+ */
---
+++ rust/src/leetcode/one_thousand_eight_hundred_forty_five.rs
@@ -0,0 +1,34 @@
+﻿use std::collections::BinaryHeap;
+
+pub struct Solution;
+
+struct SeatManager {
+    seats:i32,
+    available:BinaryHeap<i32>,
+}
+
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl SeatManager {
+
+    fn new(_: i32) -> Self {
+        Self{seats:0, available:BinaryHeap::new()}
+    }
+
+    fn reserve(&mut self) -> i32 {
+        if let Some(seat) =self.available.pop(){
+            -seat
+        }else{
+            self.seats+=1;
+            self.seats
+        }
+    }
+
+    fn unreserve(&mut self, seat_number: i32) {
+        self.available.push(-seat_number);
+    }
+}
+
