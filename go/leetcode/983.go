eb23b1db89a83ee3a02b8a42d62ea619fea843a6
catwithtudou <949812478@qq.com>
2024-10-01 11:16:36 +0800

    update: done the 983

---
+++ go/leetcode/983.go
@@ -0,0 +1,28 @@
+﻿package leetcode
+
+func mincostTickets(days []int, costs []int) int {
+	lastDay := days[len(days)-1]
+	isTravel := make([]bool, lastDay+1)
+	for _, v := range days {
+		isTravel[v] = true
+	}
+	memo := make([]int, lastDay+1)
+	var dfs func(i int) int
+	dfs = func(i int) (res int) {
+		if i <= 0 {
+			return
+		}
+		p := &memo[i]
+		if *p > 0 {
+			return *p
+		}
+		defer func() {
+			*p = res
+		}()
+		if !isTravel[i] {
+			return dfs(i - 1)
+		}
+		return min(min(dfs(i-1)+costs[0], dfs(i-7)+costs[1]), dfs(i-30)+costs[2])
+	}
+	return dfs(lastDay)
+}
---
+++ rust/src/leetcode/nine_hundred_and_eighty_three.rs
@@ -0,0 +1,22 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn mincost_tickets(days: Vec<i32>, costs: Vec<i32>) -> i32 {
+        let last_day = *days.last().unwrap() as usize;
+        let mut is_travel = vec![false; last_day + 1];
+        for &day in &days {
+            is_travel[day as usize] = true;
+        }
+        let mut f = vec![0; last_day + 1];
+        for i in 1..=last_day {
+            if !is_travel[i] {
+                f[i] = f[i - 1];
+            } else {
+                f[i] = (f[i - 1] + costs[0])
+                    .min(f[i.max(7) - 7] + costs[1])
+                    .min(f[i.max(30) - 30] + costs[2]);
+            }
+        }
+        f[last_day]
+    }
+}
