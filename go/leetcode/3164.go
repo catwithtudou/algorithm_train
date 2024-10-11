b30d4131e629534cb6f4d3c48c91e38e15d0f797
catwithtudou <949812478@qq.com>
2024-10-11 08:36:39 +0800

    update: done the 3164

---
+++ go/leetcode/3164.go
@@ -0,0 +1,25 @@
+﻿package leetcode
+
+func numberOfPairs3164(nums1 []int, nums2 []int, k int) (ans int64) {
+	cnt := make(map[int]int64, 0)
+	for _, x := range nums1 {
+		if x%k != 0 {
+			continue
+		}
+		x /= k
+		for d := 1; d*d <= x; d++ {
+			if x%d != 0 {
+				continue
+			}
+			cnt[d] += 1
+			if d*d < x {
+				cnt[x/d] += 1
+			}
+		}
+	}
+
+	for _, v := range nums2 {
+		ans += cnt[v]
+	}
+	return
+}
