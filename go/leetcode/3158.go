6a5374be3b31312ac02a3d484f36af1b22b4fc43
catwithtudou <949812478@qq.com>
2024-10-12 10:27:55 +0800

    update: done the 3158

---
+++ go/leetcode/3158.go
@@ -0,0 +1,13 @@
+﻿package leetcode
+
+func duplicateNumbersXOR(nums []int) int {
+	vis, ans := 0, 0
+	for _, x := range nums {
+		if vis>>x&1 > 0 {
+			ans ^= x
+		} else {
+			vis |= 1 << x
+		}
+	}
+	return ans
+}
---
+++ rust/src/leetcode/three_thousand_one_hundred_fifty_eight.rs
@@ -0,0 +1,16 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn duplicate_numbers_xor(nums: Vec<i32>) -> i32 {
+        let mut ans = 0;
+        let mut vis = 0i64;
+        for x in nums {
+            if (vis>>x&1) > 0 {
+                ans^=x;
+            }else{
+                vis|=1<<x;
+            }
+        }
+        return ans;
+    }
+}
