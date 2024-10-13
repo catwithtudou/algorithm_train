6186a9964f18d2180f48ac6467b11cdf6a4784d0
catwithtudou <949812478@qq.com>
2024-10-13 11:08:36 +0800

    update: done the 1884

---
+++ go/leetcode/1884.go
@@ -0,0 +1,23 @@
+﻿package leetcode
+
+import "math"
+
+var memo [1001]int
+
+func twoEggDrop(n int) int {
+	if n == 0 {
+		return 0
+	}
+	p := &memo[n]
+	if *p != 0 {
+		return *p
+	}
+
+	res := math.MaxInt
+	for i := 1; i <= n; i++ {
+		res = min(res, max(i, twoEggDrop(n-i)+1))
+	}
+	*p = res
+
+	return res
+}
---
+++ rust/src/leetcode/one_thousand_eight_hundred_eighty_four.rs
@@ -0,0 +1,20 @@
+﻿pub struct Solution;
+
+use std::cmp::max;
+use std::cmp::min;
+
+impl Solution {
+    pub fn two_egg_drop(n: i32) -> i32 {
+        let n = n as usize;
+        let mut f = vec![0; n + 1];
+
+        for i in 1..=n {
+            f[i] = i32::MAX;
+            for j in 1..=i {
+                f[i] = min(f[i], max(j as i32, f[i - j] + 1));
+            }
+        }
+
+        return f[n];
+    }
+}
