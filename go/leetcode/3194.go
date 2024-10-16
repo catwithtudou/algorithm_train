bab15c77db950b0897b034b99dd4fbf0ff404c1b
catwithtudou <949812478@qq.com>
2024-10-16 09:39:15 +0800

    update: done the 3194

---
+++ go/leetcode/3194.go
@@ -0,0 +1,16 @@
+﻿package leetcode
+
+import (
+	"math"
+
+	"golang.org/x/exp/slices"
+)
+
+func minimumAverage(nums []int) float64 {
+	slices.Sort(nums)
+	ans := math.MaxInt64
+	for i, n := 0, len(nums); i < n/2; i++ {
+		ans = min(ans, nums[i]+nums[n-1-i])
+	}
+	return float64(ans) / 2
+}
---
+++ rust/src/leetcode/three_thousand_one_hundred_ninty_four.rs
@@ -0,0 +1,9 @@
+﻿pub struct Solution;
+
+
+impl Solution {
+    pub fn minimum_average(mut nums: Vec<i32>) -> f64 {
+        nums.sort_unstable();
+        (0..nums.len()/2).map(|i| nums[i]+nums[nums.len()-1-i]).min().unwrap() as f64 / 2.0
+    }
+}
