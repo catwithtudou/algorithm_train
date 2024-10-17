f15fcd81d2719290f715387bae669a10a63d56b9
catwithtudou <949812478@qq.com>
2024-10-17 10:10:34 +0800

    update: done the 3193

---
+++ go/leetcode/3193.go
@@ -0,0 +1,36 @@
+﻿package leetcode
+
+func numberOfPermutations(n int, requirements [][]int) int {
+	req := make([]int, n)
+	for i := range req {
+		req[i] = -1
+	}
+	m := -1
+	for _, r := range requirements {
+		req[r[0]] = r[1]
+		m = max(m, r[1])
+	}
+	if req[0] > 0 {
+		return 0
+	}
+
+	f := make([][]int, n)
+	for i := range f {
+		f[i] = make([]int, m+1)
+	}
+	f[0][0] = 1
+
+	for i := 1; i < n; i++ {
+		l, r := req[i], req[i]
+		if req[i] < 0 {
+			l, r = 0, m
+		}
+		for j := l; j <= r; j++ {
+			for k := 0; k < min(i, j); k++ {
+				f[i][j] = (f[i][j] + f[i-1][j-k]) % (1e9 + 7)
+			}
+		}
+	}
+
+	return f[n-1][req[n-1]]
+}
---
+++ rust/src/leetcode/three_thousand_one_hundred_ninty_three.rs
@@ -0,0 +1,33 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn number_of_permutations(n: i32, requirements: Vec<Vec<i32>>) -> i32 {
+        let n = n as usize;
+        let mut req = vec![-1; n];
+        let mut m: usize = 0;
+        for r in requirements {
+            req[r[0] as usize] = r[1];
+            m = m.max(r[1] as usize);
+        }
+        if req[0] > 0 {
+            return 0;
+        }
+
+        let mut f = vec![vec![0; m as usize + 1]; n];
+        f[0][0] = 1;
+        for i in 1..n {
+            let (mut l, mut r) = (0 as usize, m);
+            if req[i] >= 0 {
+                l = req[i] as usize;
+                r = req[i] as usize;
+            }
+            for j in l..=r {
+                for k in 0..=i.min(j) {
+                    f[i][j] = (f[i][j] + f[i - 1][j - k]) % 1000000007;
+                }
+            }
+        }
+
+        f[n - 1][req[n - 1] as usize]
+    }
+}
