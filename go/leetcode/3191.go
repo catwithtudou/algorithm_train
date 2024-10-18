68a9af7924dd646aeabf7a359a51bec078ca7b7e
catwithtudou <949812478@qq.com>
2024-10-18 10:02:25 +0800

    update: done the 3191

---
+++ go/leetcode/3191.go
@@ -0,0 +1,18 @@
+﻿package leetcode
+
+func minOperations3191(nums []int) int {
+	ans, n := 0, len(nums)
+	for i, x := range nums[:n-2] {
+		if x == 0 {
+			nums[i+1] ^= 1
+			nums[i+2] ^= 1
+			ans++
+		}
+	}
+
+	if nums[n-2] == 0 || nums[n-1] == 0 {
+		return -1
+	}
+
+	return ans
+}
---
+++ rust/src/leetcode/three_thousand_one_hundred_ninty_one.rs
@@ -0,0 +1,21 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn min_operations(mut nums: Vec<i32>) -> i32 {
+        let mut ans = 0;
+        let n = nums.len();
+        for i in 0..n - 2 {
+            if nums[i] == 0 {
+                nums[i + 1] ^= 1;
+                nums[i + 2] ^= 1;
+                ans += 1;
+            }
+        }
+
+        if nums[n - 2] == 0 || nums[n - 1] == 0 {
+            -1
+        } else {
+            ans
+        }
+    }
+}
