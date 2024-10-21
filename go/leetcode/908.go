a4a72224846dfa331171ce628e6b57f2de982b41
catwithtudou <949812478@qq.com>
2024-10-21 10:08:50 +0800

    update: update the 908

---
+++ go/leetcode/908.go
@@ -0,0 +1,7 @@
+﻿package leetcode
+
+import "golang.org/x/exp/slices"
+
+func smallestRangeI(nums []int, k int) int {
+	return max(0, slices.Max(nums)-slices.Min(nums)-2*k)
+}
---
+++ rust/src/leetcode/nine_hundred_and_eight.rs
@@ -0,0 +1,9 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn smallest_range_i(nums: Vec<i32>, k: i32) -> i32 {
+        let mn = *nums.iter().min().unwrap();
+        let mx = *nums.iter().max().unwrap();
+        return 0.max(mx-mn-2*k);
+    }
+}
