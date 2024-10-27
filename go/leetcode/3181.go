b5a81321131326a9dcfecb8daa7789e92d7c3d02
catwithtudou <949812478@qq.com>
2024-10-27 13:33:15 +0800

    update: done the 3181

---
+++ go/leetcode/3181.go
@@ -0,0 +1,25 @@
+﻿package leetcode
+
+import (
+	"math/big"
+
+	"golang.org/x/exp/slices"
+)
+
+func maxTotalRewardII(rewardValues []int) int {
+	m := slices.Max(rewardValues)
+	if slices.Contains(rewardValues, m-1) {
+		return 2*m - 1
+	}
+	slices.Sort(rewardValues)
+	rewardValues = slices.Compact(rewardValues)
+
+	one := big.NewInt(1)
+	f := big.NewInt(1)
+	p := new(big.Int)
+	for _, v := range rewardValues {
+		mask := p.Sub(p.Lsh(one, uint(v)), one)
+		f.Or(f, p.Lsh(p.And(f, mask), uint(v)))
+	}
+	return f.BitLen() - 1
+}
