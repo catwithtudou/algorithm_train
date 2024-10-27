3f4446393b5da0b5e5d0019d0966f4f90ddbaf95
catwithtudou <949812478@qq.com>
2024-10-27 12:54:15 +0800

    update: done the 684

---
+++ go/leetcode/684.go
@@ -0,0 +1,30 @@
+﻿package leetcode
+
+func findRedundantConnection(edges [][]int) []int {
+	parents := make([]int, len(edges)+1)
+	for i := range parents {
+		parents[i] = i
+	}
+	var find func(x int) int
+	find = func(x int) int {
+		if parents[x] != x {
+			parents[x] = find(parents[x])
+		}
+		return parents[x]
+	}
+	var union func(from, to int) bool
+	union = func(from, to int) bool {
+		x, y := find(from), find(to)
+		if x == y {
+			return false
+		}
+		parents[x] = y
+		return true
+	}
+	for _, e := range edges {
+		if !union(e[0], e[1]) {
+			return e
+		}
+	}
+	return nil
+}
---
+++ rust/src/leetcode/six_hundred_and_eighty_four.rs
@@ -0,0 +1,34 @@
+﻿pub struct Solution;
+
+
+impl Solution {
+    pub fn find_redundant_connection(edges: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut parent: Vec<_> = (0..=1000).collect();
+        let mut ans = vec![];
+
+        fn find(parent: &mut Vec<usize>, mut idx: usize) -> usize {
+            while parent[idx] != idx {
+                parent[idx] = parent[parent[idx]];
+                idx = parent[idx];
+            }
+            idx
+        }
+
+        fn union(parent: &mut Vec<usize>, from: usize, to: usize) -> bool {
+            let (from, to) = (find(parent, from), find(parent, to));
+            if from == to {
+                return false;
+            }
+            parent[from] = to;
+            true
+        }
+
+        for edge in edges.iter() {
+            if !union(&mut parent, edge[0] as usize, edge[1] as usize){
+                ans = edge.clone();
+                break;
+            }
+        }
+        ans
+    }
+}
