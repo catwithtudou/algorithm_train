6042b180be642c6ea364c41f640d2950be057186
catwithtudou <949812478@qq.com>
2024-10-29 08:31:49 +0800

    update: done the 3211

---
+++ go/leetcode/3211.go
@@ -0,0 +1,22 @@
+﻿package leetcode
+
+func validStrings(n int) []string {
+	ans := make([]string, 0)
+	path := make([]byte, n)
+	var dfs func(i int)
+	dfs = func(i int) {
+		if i == n {
+			ans = append(ans, string(path))
+			return
+		}
+		path[i] = '1'
+		dfs(i + 1)
+
+		if i == 0 || path[i-1] == '1' {
+			path[i] = '0'
+			dfs(i + 1)
+		}
+	}
+	dfs(0)
+	return ans
+}
---
+++ rust/src/leetcode/three_thousand_two_hundred_and_eleven.rs
@@ -0,0 +1,8 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn valid_strings(n: i32) -> Vec<String> {
+        let mask = (1<<n)-1;
+        (0..1<<n).filter(|&x| (x>>1)&x==0).map(|x| format!("{:0w$b}",x^mask,w==n as usize)).collect()
+    }
+}
