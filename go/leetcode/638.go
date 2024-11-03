58f0c8973345e7fe7fbec9f767671d302133af9a
catwithtudou <949812478@qq.com>
2024-11-03 10:25:36 +0800

    update: done the 638

---
+++ go/leetcode/638.go
@@ -0,0 +1,40 @@
+﻿package leetcode
+
+func shoppingOffers(price []int, special [][]int, needs []int) int {
+	const bits = 4
+	n := len(needs)
+	mask := 0
+	for i, need := range needs {
+		mask |= need << (i * bits)
+	}
+	f := make(map[int]int)
+	var dfs func(int) int
+	dfs = func(cur int) int {
+		if v, ok := f[cur]; ok {
+			return v
+		}
+
+		ans := 0
+		for i := 0; i < n; i++ {
+			ans += price[i] * ((cur >> (i * bits)) & 0xf)
+		}
+		for _, offer := range special {
+			nxt := cur
+			ok := true
+			for j := 0; j < n; j++ {
+				if ((cur >> (j * bits)) & 0xf) < offer[j] {
+					ok = false
+					break
+				}
+				nxt -= offer[j] << (j * bits)
+			}
+			if ok {
+				ans = min(ans, offer[n]+dfs(nxt))
+			}
+
+		}
+		f[cur] = ans
+		return ans
+	}
+	return dfs(mask)
+}
