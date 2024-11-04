3336447b45c3e912332dbabb32fa42e119d2b30e
catwithtudou <949812478@qq.com>
2024-11-04 09:34:32 +0800

    update: done the 633

---
+++ go/leetcode/633.go
@@ -0,0 +1,13 @@
+﻿package leetcode
+
+import "math"
+
+func judgeSquareSum(c int) bool {
+	for a := 0; a*a <= c/2; a++ {
+		b := int(math.Sqrt(float64(c - a*a)))
+		if a*a+b*b == c {
+			return true
+		}
+	}
+	return false
+}
---
+++ rust/src/leetcode/six_hundred_and_thirty_three.rs
@@ -0,0 +1,19 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn judge_square_sum(c: i32) -> bool {
+        let mut a = 0;
+        let mut b = (c as f64).sqrt() as i32;
+        while a <= b {
+            if a * a == c - b * b {
+                return true;
+            } else if a * a < c - b * b {
+                a += 1;
+            } else {
+                b -= 1;
+            }
+        }
+
+        false
+    }
+}
