436d19e430486f66985deb1274c09775b5b5f1a9
catwithtudou <949812478@qq.com>
2024-11-11 10:01:45 +0800

    update: done the 1543

---
+++ go/leetcode/1547.go
@@ -0,0 +1,36 @@
+﻿package leetcode
+
+import (
+	"math"
+
+	"golang.org/x/exp/slices"
+)
+
+func minCost1547(n int, cuts []int) int {
+	cuts = append(cuts, 0, n)
+	slices.Sort(cuts)
+
+	m := len(cuts)
+	memo := make([][]int, m)
+	for i := range memo {
+		memo[i] = make([]int, m)
+	}
+	var dfs func(int, int) int
+	dfs = func(i, j int) int {
+		if i+1 == j {
+			return 0
+		}
+		p := &memo[i][j]
+		if *p != 0 {
+			return *p
+		}
+		res := math.MaxInt
+		for k := i + 1; k < j; k++ {
+			res = min(res, dfs(i, k)+dfs(k, j))
+		}
+		*p = res + cuts[j] - cuts[i]
+		return *p
+	}
+
+	return dfs(0, m-1)
+}
---
+++ rust/src/leetcode/one_thousand_five_hundred_forty_seven.rs
@@ -0,0 +1,22 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn min_cost(n: i32, mut cuts: Vec<i32>) -> i32 {
+        cuts.push(0);
+        cuts.push(n);
+        cuts.sort_unstable();
+
+        let m = cuts.len();
+        let mut f = vec![vec![0; m]; m];
+        for i in (0..m - 2).rev() {
+            for j in i + 2..m {
+                let mut res = i32::MAX;
+                for k in i + 1..j {
+                    res = res.min(f[i][k] + f[k][j]);
+                }
+                f[i][j] = res + cuts[j] - cuts[i];
+            }
+        }
+        f[0][m - 1]
+    }
+}
