2365489568750bdfa72a2144e120730b470d6dde
catwithtudou <949812478@qq.com>
2024-11-16 10:59:18 +0800

    update: done the 3240

--- rust/src/leetcode.rs
+++ rust/src/leetcode.rs
@@ -255,6 +255,7 @@ pub mod three_thousand_two_hundred_and_sixteen;
 pub mod three_thousand_two_hundred_twenty_two;
 pub mod three_thousand_two_hundred_twenty_six;
 pub mod three_thousand_two_hundred_thirty_nine;
+pub mod three_thousand_two_hundred_and_forty;
 pub mod three_thousand_two_hundred_forty_two;
 pub mod three_thousand_two_hundred_fifty_four;
 pub mod three_thousand_two_hundred_fifty_five;
