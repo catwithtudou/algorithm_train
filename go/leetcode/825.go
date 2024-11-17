fd244a0c647feef69f0a7b16d661909430440d7a
catwithtudou <949812478@qq.com>
2024-11-17 09:52:34 +0800

    update: done the 825

---
+++ go/leetcode/825.go
@@ -0,0 +1,22 @@
+﻿package leetcode
+
+func numFriendRequests(ages []int) int {
+	cnt := [121]int{}
+	for _, age := range ages {
+		cnt[age]++
+	}
+
+	ans := 0
+	cntWindows, ageY := 0, 0
+	for ageX, c := range cnt {
+		cntWindows += c
+		if ageY*2 <= ageX+14 {
+			cntWindows -= cnt[ageY]
+			ageY++
+		}
+		if cntWindows > 0 {
+			ans += c*cntWindows - c
+		}
+	}
+	return ans
+}
