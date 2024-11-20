c835d21b0065dd8bee83b5f671502f114b69ba07
catwithtudou <949812478@qq.com>
2024-11-20 09:31:07 +0800

    update: update the reme

--- rust/src/leetcode/three_thousand_two_hundred_forty_four.rs
+++ rust/src/leetcode/three_thousand_two_hundred_forty_four.rs
@@ -2,17 +2,18 @@
 
 impl Solution {
     pub fn shortest_distance_after_queries(n: i32, queries: Vec<Vec<i32>>) -> Vec<i32> {
-        let mut nxt: Vec<i32> = (1..n - 1).collect();
+        let mut nxt: Vec<i32> = (1..n).collect();
         let mut ans = vec![0; queries.len()];
         let mut cnt = n - 1;
         for (i, q) in queries.iter().enumerate() {
             let (l, r) = (q[0] as usize, q[1] as usize);
             if nxt[l] > 0 && nxt[l] < r as i32 {
                 let mut j = nxt[l] as usize;
-                while j < r  {
+                while j < r {
                     cnt -= 1;
-                    j = nxt[j] as usize;
+                    let tmp = nxt[j] as usize;
                     nxt[j] = 0;
+                    j = tmp;
                 }
                 nxt[l] = r as i32;
             }
