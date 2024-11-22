700b78a29e5dcf05f90c80622c966bd04900b376
catwithtudou <949812478@qq.com>
2024-11-23 05:54:35 +0800

    update: done the 3238

---
+++ go/leetcode/3238.go
@@ -0,0 +1,18 @@
+﻿package leetcode
+
+func winningPlayerCount(n int, pick [][]int) int {
+	cnts := make([][11]int, n)
+	for _, p := range pick {
+		cnts[p[0]][p[1]]++
+	}
+	ans := 0
+	for i, cnt := range cnts {
+		for _, c := range cnt {
+			if c > i {
+				ans++
+				break
+			}
+		}
+	}
+	return ans
+}
---
+++ rust/src/leetcode/three_thousand_two_hundred_thirty_eight.rs
@@ -0,0 +1,15 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn winning_player_count(n: i32, pick: Vec<Vec<i32>>) -> i32 {
+        let mut cnts = vec![vec![0; 11]; n as usize];
+        for p in pick {
+            cnts[p[0] as usize][p[1] as usize] += 1;
+        }
+
+        cnts.iter()
+            .enumerate()
+            .filter(|(i, cnt)| cnt.iter().any(|c| c > i))
+            .count() as _
+    }
+}
