b2773dce614716312e4ceee35eabc53407d14be3
catwithtudou <949812478@qq.com>
2024-11-22 09:44:53 +0800

    update: done the 3233

---
+++ go/leetcode/3233.go
@@ -0,0 +1,26 @@
+﻿package leetcode
+
+import "math"
+
+const mx3233 = 31622
+
+var pi [mx + 1]int
+
+func init() {
+	for i := 2; i <= mx; i++ {
+		if pi[i] == 0 {
+			pi[i] = pi[i-1] + 1
+			for j := i * i; j <= mx; j += i {
+				pi[j] = -1
+			}
+		} else {
+			pi[i] = pi[i-1]
+		}
+	}
+}
+
+func nonSpecialCount(l int, r int) int {
+	rCnt := pi[int(math.Sqrt(float64(r)))]
+	lCnt := pi[int(math.Sqrt(float64(l-1)))]
+	return r - l + 1 - (rCnt - lCnt)
+}
---
+++ rust/src/leetcode/three_thousand_two_hundred_thirty_three.rs
@@ -0,0 +1,25 @@
+﻿pub struct Solution;
+
+// Compare similar snippet from rust/src/leetcode/three_thousand_two_hundred_thirty_four.rs:
+impl Solution {
+    pub fn non_special_count(l: i32, r: i32) -> i32 {
+        const MX: usize = 31622; // 使用常量
+        let mut pi = [0; MX + 1];
+        for i in 2..=MX {
+            if pi[i] == 0 {
+                pi[i] = pi[i - 1] + 1;
+                let mut j = i * i;
+                while j <= MX {
+                    pi[j] = -1;
+                    j += i;
+                }
+            } else {
+                pi[i] = pi[i - 1];
+            }
+        }
+
+        let r_cnt = pi[f64::sqrt(r as f64) as usize];
+        let l_cnt = pi[f64::sqrt((l - 1) as f64) as usize];
+        r - l + 1 - (r_cnt - l_cnt)
+    }
+}
