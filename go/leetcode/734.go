3ecee03dc925e8b639c30cf3ae97735db2e74a54
catwithtudou <949812478@qq.com>
2024-11-25 09:48:28 +0800

    update: done the 743

---
+++ go/leetcode/734.go
@@ -0,0 +1,47 @@
+﻿package leetcode
+
+import (
+	"math"
+
+	"golang.org/x/exp/slices"
+)
+
+func networkDelayTime(times [][]int, n int, k int) int {
+	const inf = math.MaxInt / 2
+	g := make([][]int, n)
+	for i := range g {
+		g[i] = make([]int, n)
+		for j := range g[i] {
+			g[i][j] = inf
+		}
+	}
+	for _, t := range times {
+		g[t[0]-1][t[1]-1] = t[2]
+	}
+
+	dis := make([]int, n)
+	for i := range dis {
+		dis[i] = inf
+	}
+	dis[k-1] = 0
+	done := make([]bool, n)
+	for {
+		x := -1
+		for i, ok := range done {
+			if !ok && (x < 0 || dis[i] < dis[x]) {
+				x = i
+			}
+		}
+		if x < 0 {
+			return slices.Max(dis)
+		}
+		if dis[x] == inf {
+			return -1
+		}
+		done[x] = true
+		for y, d := range g[x] {
+			dis[y] = min(dis[y], dis[x]+d)
+		}
+
+	}
+}
---
+++ rust/src/leetcode/seven_hundred_and_forty_three.rs
@@ -0,0 +1,36 @@
+﻿use std::collections::BinaryHeap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn network_delay_time(times: Vec<Vec<i32>>, n: i32, k: i32) -> i32 {
+        let n = n as usize;
+        let k = k as usize - 1;
+        let mut g = vec![vec![]; n];
+        for t in &times {
+            g[t[0] as usize - 1].push((t[1] as usize - 1, t[2]));
+        }
+        let mut dis = vec![i32::MAX; n];
+        dis[k] = 0;
+        let mut h = BinaryHeap::new();
+        h.push((0, k));
+        while let Some((dx, x)) = h.pop() {
+            if -dx > dis[x] {
+                continue;
+            }
+            for &(y, d) in &g[x] {
+                let new_dis = -dx + d;
+                if new_dis < dis[y] {
+                    dis[y] = new_dis;
+                    h.push((-new_dis, y));
+                }
+            }
+        }
+        let mx = *dis.iter().max().unwrap();
+        if mx < i32::MAX {
+            mx
+        } else {
+            -1
+        }
+    }
+}
