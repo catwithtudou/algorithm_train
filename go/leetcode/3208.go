0947a14e02503944e37f68a9db96bb64e7044385
catwithtudou <949812478@qq.com>
2024-11-27 08:51:05 +0800

    update: done the 3208

---
+++ go/leetcode/3208.go
@@ -0,0 +1,16 @@
+﻿package leetcode
+
+func numberOfAlternatingGroupsII(colors []int, k int) int {
+	ans, cnt := 0, 0
+	n := len(colors)
+	for i := 0; i < 2*n; i++ {
+		if i > 0 && colors[i%n] == colors[(i-1)%n] {
+			cnt = 0
+		}
+		cnt++
+		if i >= n && cnt >= k {
+			ans++
+		}
+	}
+	return ans
+}
---
+++ rust/src/leetcode/three_thousand_two_hundred_and_eight.rs
@@ -0,0 +1,18 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn number_of_alternating_groups(colors: Vec<i32>, k: i32) -> i32 {
+        let (mut cnt, mut ans) = (0, 0);
+        let n = colors.len();
+        for i in 0..2 * n {
+            if i > 0 && colors[i % n] == colors[(i - 1) % n] {
+                cnt = 0;
+            }
+            cnt += 1;
+            if i >= n && cnt >= k {
+                ans += 1;
+            }
+        }
+        ans
+    }
+}
