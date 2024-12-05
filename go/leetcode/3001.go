0cd76ba0bf2aa062fc16598abe479b8874048c40
catwithtudou <949812478@qq.com>
2024-12-05 09:16:10 +0800

    update: done the 3001

---
+++ go/leetcode/3001.go
@@ -0,0 +1,24 @@
+﻿package leetcode
+
+func minMovesToCaptureTheQueen(a int, b int, c int, d int, e int, f int) int {
+	isBetween := func(l, m, r int) bool {
+		return min(l, r) < m && m < max(l, r)
+	}
+
+	if a == e && (c != e || !isBetween(b, d, f)) {
+		return 1
+	}
+	if b == f && (d != f || !isBetween(a, c, e)) {
+		return 1
+	}
+
+	if c+d == e+f && (a+b != c+d || !isBetween(c, a, e)) {
+		return 1
+	}
+
+	if c-d == e-f && (a-b != c-d || !isBetween(c, a, e)) {
+		return 1
+	}
+
+	return 2
+}
---
+++ rust/src/leetcode/three_thousand_and_one.rs
@@ -0,0 +1,24 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn min_moves_to_capture_the_queen(a: i32, b: i32, c: i32, d: i32, e: i32, f: i32) -> i32 {
+        let in_between = |l: i32, m: i32, r: i32| l.min(r) < m && m < l.max(r);
+
+        if a == e && (c != e || !in_between(b, d, f)) {
+            return 1;
+        }
+        if b == f && (d != f || !in_between(a, c, e)) {
+            return 1;
+        }
+
+        if c + d == e + f && (a + b != e + f || !in_between(c, a, e)) {
+            return 1;
+        }
+
+        if c-d==e-f&&(a-b!=e-f||!in_between(c, a, e)){
+            return 1;
+        }
+
+        2
+    }
+}
