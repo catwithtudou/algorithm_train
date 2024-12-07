41e1785a810f248b428f276a34319723f518671d
catwithtudou <949812478@qq.com>
2024-12-07 09:00:04 +0800

    update: done the 688

---
+++ go/leetcode/688.go
@@ -0,0 +1,33 @@
+﻿package leetcode
+
+func knightProbability(n int, k int, row int, column int) float64 {
+	dirs := []struct{ x, y int }{{-2, -1}, {-1, -2}, {1, -2}, {2, -1}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1}}
+	memo := make([][][]float64, k+1)
+	for i := range memo {
+		memo[i] = make([][]float64, n)
+		for j := range memo[i] {
+			memo[i][j] = make([]float64, n)
+		}
+	}
+	var dfs func(int, int, int) float64
+	dfs = func(k, i, j int) float64 {
+		if i < 0 || j < 0 || i >= n || j >= n {
+			return 0
+		}
+		if k == 0 {
+			return 1
+		}
+		p := &memo[k][i][j]
+		if *p != 0 {
+			return *p
+		}
+		res := 0.0
+		for _, d := range dirs {
+			res += dfs(k-1, i+d.x, j+d.y)
+		}
+		res /= 8
+		*p = res
+		return res
+	}
+	return dfs(k, row, column)
+}
---
+++ rust/src/leetcode/six_hundred_and_eighty_eight.rs
@@ -0,0 +1,47 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn knight_probability(n: i32, k: i32, row: i32, column: i32) -> f64 {
+        let dirs = [
+            (1, 2),
+            (2, 1),
+            (2, -1),
+            (1, -2),
+            (-1, -2),
+            (-2, -1),
+            (-2, 1),
+            (-1, 2),
+        ];
+        let mut memo = vec![vec![vec![0.0 as f64; n as usize]; n as usize]; k as usize + 1];
+
+        fn dfs(
+            i: i32,
+            j: i32,
+            k: i32,
+            n: i32,
+            dirs: &[(i32, i32)],
+            memo: &mut Vec<Vec<Vec<f64>>>,
+        ) -> f64 {
+            if i < 0 || j < 0 || i >= n || j >= n {
+                return 0.0;
+            }
+            if k == 0 {
+                return 1.0;
+            }
+
+            if memo[k as usize][i as usize][j as usize] > 0.0 {
+                return memo[k as usize][i as usize][j as usize];
+            }
+
+            let mut res = 0f64;
+            for &(di, dj) in dirs {
+                res += dfs(i + di, j + dj, k - 1, n, dirs, memo);
+            }
+            res /= 8.0;
+            memo[k as usize][i as usize][j as usize] = res;
+            res
+        }
+
+        dfs(row, column, k, n, &dirs, &mut memo)
+    }
+}
