c00fc9eb0ae067be2fcd096154aeaa80ed0c68c8
catwithtudou <949812478@qq.com>
2024-12-12 09:03:41 +0800

    update: done the 2931

---
+++ go/leetcode/2931.go
@@ -0,0 +1,41 @@
+﻿package leetcode
+
+import (
+	"container/heap"
+	"sort"
+)
+
+func maxSpending(values [][]int) (ans int64) {
+	m, n := len(values), len(values[0])
+	idx := make([]int, m)
+	for i := range idx {
+		idx[i] = i
+	}
+	h := &spHp{idx, values}
+	heap.Init(h)
+
+	for d := 1; d <= m*n; d++ {
+		a := values[idx[0]]
+		ans += int64(a[len(a)-1]) * int64(d)
+		if len(a) > 1 {
+			values[idx[0]] = a[:len(a)-1]
+			heap.Fix(h, 0)
+		} else {
+			heap.Pop(h)
+		}
+	}
+
+	return
+}
+
+type spHp struct {
+	sort.IntSlice
+	values [][]int
+}
+
+func (h spHp) Less(i, j int) bool {
+	a, b := h.values[h.IntSlice[i]], h.values[h.IntSlice[j]]
+	return a[len(a)-1] < b[len(b)-1]
+}
+func (spHp) Push(any)        {}
+func (h *spHp) Pop() (_ any) { a := h.IntSlice; h.IntSlice = a[:len(a)-1]; return }
---
+++ rust/src/leetcode/two_thousand_nine_hundred_thrity_one.rs
@@ -0,0 +1,18 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn max_spending(values: Vec<Vec<i32>>) -> i64 {
+        let mut count = Vec::new();
+        let mut ans = 0i64;
+        for i in &values {
+            for &v in i {
+                count.push(v);
+            }
+        }
+        count.sort();
+        for i in 0..count.len() {
+            ans += count[i] as i64 * (i + 1) as i64;
+        }
+        ans
+    }
+}
