a2f7d419c43f84b32dfc2659754f97f6466b94f9
catwithtudou <949812478@qq.com>
2024-12-13 08:55:22 +0800

    update: done the 3264

---
+++ go/leetcode/3264.go
@@ -0,0 +1,62 @@
+﻿package leetcode
+
+import (
+	"container/heap"
+	"sort"
+)
+
+func getFinalState(nums []int, k int, multiplier int) []int {
+	if multiplier == 1 {
+		return nums
+	}
+
+	n := len(nums)
+	h := make(sHp, n)
+	mx := 0
+	for i, x := range nums {
+		h[i] = sPair{x, i}
+		mx = max(mx, x)
+	}
+	heap.Init(&h)
+
+	for ; k > 0 && h[0].x < mx; k-- {
+		h[0].x *= multiplier
+		heap.Fix(&h, 0)
+	}
+
+	sort.Slice(h, func(i, j int) bool {
+		return less(h[i], h[j])
+	})
+	for i, p := range h {
+		e := k / n
+		if i < k%n {
+			e++
+		}
+		nums[p.i] = p.x % mod * calQuickPow(multiplier, e) % mod
+	}
+
+	return nums
+}
+
+type sPair struct{ x, i int }
+
+func less(a, b sPair) bool { return a.x < b.x || a.x == b.x && a.i < b.i }
+
+type sHp []sPair
+
+func (s sHp) Len() int           { return len(s) }
+func (s sHp) Less(i, j int) bool { return less(s[i], s[j]) }
+func (s sHp) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s *sHp) Push(v any)        {}
+func (s *sHp) Pop() (_ any)      { return }
+
+func calQuickPow(x, n int) int {
+	res := 1
+	for ; n > 0; n /= 2 {
+		if n%2 > 0 {
+			res = res * x % mod
+		}
+		x = x * x % mod
+	}
+	return res
+}
---
+++ rust/src/leetcode/three_thousand_two_hundred_sixty_four.rs
@@ -0,0 +1,103 @@
+﻿use std::collections::BinaryHeap;
+use std::cmp::Reverse;
+
+const MOD: i64 = 1_000_000_007;
+
+#[derive(Debug, Eq, PartialEq)]
+struct Pair {
+    x: i64,
+    i: usize,
+}
+
+impl PartialOrd for Pair {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Pair {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self.x.cmp(&other.x) {
+            std::cmp::Ordering::Equal => self.i.cmp(&other.i),
+            ord => ord,
+        }
+    }
+}
+
+pub struct Solution;
+
+impl Solution {
+    pub fn get_final_state(nums: Vec<i32>, k: i32, multiplier: i32) -> Vec<i32> {
+        if multiplier == 1 {
+            return nums;
+        }
+
+        let n = nums.len();
+        let mut nums: Vec<i64> = nums.into_iter().map(|x| x as i64).collect();
+        let multiplier = multiplier as i64;
+        let k = k as i64;
+
+        // 创建最小堆
+        let mut heap: BinaryHeap<Reverse<Pair>> = BinaryHeap::new();
+        let mut mx = 0i64;
+
+        for (i, &x) in nums.iter().enumerate() {
+            heap.push(Reverse(Pair { x, i }));
+            mx = mx.max(x);
+        }
+
+        // 处理前k个最小值
+        let mut k_remaining = k;
+        while k_remaining > 0 && heap.peek().map_or(false, |p| p.0.x < mx) {
+            if let Some(Reverse(mut pair)) = heap.pop() {
+                // 修改这里的乘法和取余操作
+                pair.x = Self::mul_mod(pair.x, multiplier);
+                heap.push(Reverse(pair));
+                k_remaining -= 1;
+            }
+        }
+
+        // 将堆转换为有序数组
+        let mut pairs: Vec<Pair> = heap.into_iter().map(|r| r.0).collect();
+        pairs.sort_by(|a, b| {
+            if a.x != b.x {
+                a.x.cmp(&b.x)
+            } else {
+                a.i.cmp(&b.i)
+            }
+        });
+
+        // 计算最终结果
+        for (i, pair) in pairs.iter().enumerate() {
+            let mut e = k_remaining / (n as i64);
+            if i < (k_remaining % (n as i64)) as usize {
+                e += 1;
+            }
+            // 修改这里的乘法和取余操作
+            nums[pair.i] = Self::mul_mod(pair.x, Self::quick_pow(multiplier, e));
+        }
+
+        nums.into_iter().map(|x| x as i32).collect()
+    }
+
+    // 安全的模乘运算
+    fn mul_mod(a: i64, b: i64) -> i64 {
+        let a = a % MOD;
+        let b = b % MOD;
+        ((a as i128 * b as i128) % MOD as i128) as i64
+    }
+
+    // 修改后的快速幂实现
+    fn quick_pow(mut x: i64, mut n: i64) -> i64 {
+        x = x % MOD;
+        let mut res = 1i64;
+        while n > 0 {
+            if n % 2 > 0 {
+                res = Self::mul_mod(res, x);
+            }
+            x = Self::mul_mod(x, x);
+            n /= 2;
+        }
+        res
+    }
+}
