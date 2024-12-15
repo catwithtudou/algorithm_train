75d5f19a648abc21e8da25dfc5200068589cd51b
catwithtudou <949812478@qq.com>
2024-12-15 08:32:13 +0800

    update: done the 1338

---
+++ go/leetcode/1338.go
@@ -0,0 +1,27 @@
+﻿package leetcode
+
+import (
+	"maps"
+	"slices"
+)
+
+func minSetSize(arr []int) int {
+	freq := make(map[int]int, 0)
+	for _, v := range arr {
+		freq[v]++
+	}
+
+	cnt := slices.SortedFunc(maps.Values(freq), func(i, j int) int {
+		return j - i
+	})
+
+	nums := 0
+	for i, c := range cnt {
+		nums += c
+		if nums >= len(arr)/2 {
+			return i + 1
+		}
+	}
+
+	return -1
+}
