f039e6087380247b70e4913b3321fab6238bc844
catwithtudou <949812478@qq.com>
2024-12-22 09:32:57 +0800

    update: done the 1387

---
+++ go/leetcode/1387.go
@@ -0,0 +1,36 @@
+﻿package leetcode
+
+import (
+	"cmp"
+	"slices"
+)
+
+func getKth(lo int, hi int, k int) int {
+	memo := make(map[int]int, 0)
+
+	var dfs func(int) int
+	dfs = func(i int) int {
+		if i == 1 {
+			return 0
+		}
+		if res, ok := memo[i]; ok {
+			return res
+		}
+		if i%2 == 0 {
+			memo[i] = dfs(i/2) + 1
+		} else {
+			memo[i] = dfs((i*3+1)/2) + 2
+		}
+		return memo[i]
+	}
+
+	nums := make([]int, hi-lo+1)
+	for i := range nums {
+		nums[i] = lo + i
+	}
+
+	slices.SortFunc(nums, func(x, y int) int {
+		return cmp.Or(dfs(x)-dfs(y), x-y)
+	})
+	return nums[k-1]
+}
---
+++ rust/src/leetcode/one_thousand_three_hundred_eighty_seven.rs
@@ -0,0 +1,41 @@
+﻿pub struct Solution;
+
+use std::cmp::Ordering;
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn get_kth(lo: i32, hi: i32, k: i32) -> i32 {
+        let mut memo: HashMap<i32, i32> = HashMap::new();
+
+        fn dfs(i: i32, memo: &mut HashMap<i32, i32>) -> i32 {
+            if i == 1 {
+                return 0;
+            }
+
+            if let Some(&res) = memo.get(&i) {
+                return res;
+            }
+
+            let result = if i % 2 == 0 {
+                dfs(i / 2, memo) + 1
+            } else {
+                dfs((i * 3 + 1) / 2, memo) + 2
+            };
+            memo.insert(i, result);
+            result
+        }
+
+        let mut nums: Vec<i32> = (lo..=hi).collect();
+
+        nums.sort_by(|&x, &y| {
+            let dx = dfs(x, &mut memo);
+            let dy = dfs(y, &mut memo);
+            match dx.cmp(&dy) {
+                Ordering::Equal => x.cmp(&y),
+                ord => ord,
+            }
+        });
+
+        nums[k as usize - 1]
+    }
+}
