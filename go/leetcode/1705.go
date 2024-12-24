e2d87ffe789cd7e7b7a406913733d399b53f3111
catwithtudou <949812478@qq.com>
2024-12-24 09:59:58 +0800

    update: done the 1705

---
+++ go/leetcode/1705.go
@@ -0,0 +1,45 @@
+﻿package leetcode
+
+import "container/heap"
+
+func eatenApples(apples []int, days []int) (ans int) {
+	hp := appleHp{}
+	for i, num := range apples {
+		for len(hp) > 0 && hp[0].day == i {
+			heap.Pop(&hp)
+		}
+		if num > 0 {
+			heap.Push(&hp, applePair{num, i + days[i]})
+		}
+		if len(hp) > 0 {
+			ans++
+			hp[0].num--
+			if hp[0].num == 0 {
+				heap.Pop(&hp)
+			}
+		}
+	}
+
+	i := len(apples)
+	for {
+		for len(hp) > 0 && hp[0].day <= i {
+			heap.Pop(&hp)
+		}
+		if len(hp) == 0 || hp[0].num == 0 {
+			return
+		}
+		k := min(hp[0].num, hp[0].day-i)
+		heap.Pop(&hp)
+		ans += k
+		i += k
+	}
+}
+
+type applePair struct{ num, day int }
+type appleHp []applePair
+
+func (h appleHp) Len() int            { return len(h) }
+func (h appleHp) Less(i, j int) bool  { return h[i].day < h[j].day }
+func (h appleHp) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *appleHp) Push(v interface{}) { *h = append(*h, v.(applePair)) }
+func (h *appleHp) Pop() interface{}   { a := *h; v := a[len(a)-1]; *h = a[:len(a)-1]; return v }
---
+++ rust/src/leetcode/one_thousand_seven_hundred_and_five.rs
@@ -0,0 +1,48 @@
+﻿pub struct Solution;
+
+use std::collections::BinaryHeap;
+
+impl Solution {
+    pub fn eaten_apples(apples: Vec<i32>, days: Vec<i32>) -> i32 {
+        let n = apples.len();
+        let mut hp: BinaryHeap<(i32, i32)> = BinaryHeap::new();
+        let mut ans = 0;
+        for (i, (num, day)) in apples.into_iter().zip(days).enumerate() {
+            while let Some(&(r_day, _)) = hp.peek() {
+                if -r_day > i as i32 {
+                    break;
+                }
+                hp.pop();
+            }
+            if num > 0 {
+                hp.push((-(i as i32 + day), num));
+            }
+            if let Some(mut p) = hp.peek_mut() {
+                ans += 1;
+                p.1 -= 1;
+            }
+            if let Some(&(_, num)) = hp.peek() {
+                if num == 0 {
+                    hp.pop();
+                }
+            }
+        }
+
+        let mut i = n as i32;
+        loop {
+            while let Some(&(r_day, _)) = hp.peek() {
+                if -r_day > i {
+                    break;
+                }
+                hp.pop();
+            }
+            if let Some((r_day, num)) = hp.pop() {
+                let k = num.min(-r_day - i);
+                ans += k;
+                i += k;
+            } else {
+                return ans;
+            }
+        }
+    }
+}
