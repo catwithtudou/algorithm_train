b492b90866fde0e2f1c52c4af0ec6400e2d6c06d
catwithtudou <949812478@qq.com>
2024-12-25 10:23:19 +0800

    update: done the 3218

---
+++ go/leetcode/3218.go
@@ -0,0 +1,26 @@
+﻿package leetcode
+
+import "slices"
+
+func minimumCost(m int, n int, horizontalCut []int, verticalCut []int) (ans int) {
+	slices.SortFunc(horizontalCut, func(a, b int) int {
+		return b - a
+	})
+	slices.SortFunc(verticalCut, func(a, b int) int {
+		return b - a
+	})
+	i, j := 0, 0
+	h, v := 1, 1
+	for i < m-1 || j < n-1 {
+		if j == n-1 || (i < m-1 && horizontalCut[i] > verticalCut[j]) {
+			ans += horizontalCut[i] * v
+			i++
+			h++
+		} else {
+			ans += verticalCut[j] * h
+			j++
+			v++
+		}
+	}
+	return
+}
---
+++ rust/src/leetcode/three_thousand_two_hundred_and_eighteen.rs
@@ -0,0 +1,26 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn minimum_cost(
+        m: i32,
+        n: i32,
+        mut horizontal_cut: Vec<i32>,
+        mut vertical_cut: Vec<i32>,
+    ) -> i32 {
+        let (m, n) = (m as usize, n as usize);
+        horizontal_cut.sort_unstable();
+        vertical_cut.sort_unstable();
+        let mut ans = 0;
+        let (mut i, mut j) = (0, 0);
+        for _ in 0..m + n - 2 {
+            if j==n-1 || i<m-1 && horizontal_cut[i] < vertical_cut[j] {
+                ans+= horizontal_cut[i]*(n-j) as i32;
+                i+=1;
+            }else{
+                ans+= vertical_cut[j]*(m-i) as i32;
+                j+=1;
+            }
+        }
+        ans
+    }
+}
