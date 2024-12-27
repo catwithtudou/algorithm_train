51d0c3da478563ffb17e3bcfca4a7b37dbddc26f
catwithtudou <949812478@qq.com>
2024-12-27 10:30:10 +0800

    update: done the 3159

---
+++ go/leetcode/3159.go
@@ -0,0 +1,20 @@
+﻿package leetcode
+
+func occurrencesOfElement(nums []int, queries []int, x int) []int {
+	record := make(map[int]int, len(nums))
+	cnt := 0
+	for i, v := range nums {
+		if v == x {
+			cnt++
+			record[cnt] = i
+		}
+	}
+	ans := make([]int, len(queries))
+	for i, v := range queries {
+		ans[i] = -1
+		if v, ok := record[v]; ok {
+			ans[i] = v
+		}
+	}
+	return ans
+}
---
+++ rust/src/leetcode/three_thousand_one_hundred_fifty_nine.rs
@@ -0,0 +1,22 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn occurrences_of_element(nums: Vec<i32>, mut queries: Vec<i32>, x: i32) -> Vec<i32> {
+        let mut pos = Vec::new();
+        for (i, &num) in nums.iter().enumerate() {
+            if num == x {
+                pos.push(i);
+            }
+        }
+
+        for i in 0..queries.len() {
+            if queries[i] > pos.len() as i32 {
+                queries[i] = -1;
+            } else {
+                queries[i] = pos[(queries[i] - 1) as usize] as i32;
+            }
+        }
+
+        queries
+    }
+}
