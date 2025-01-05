ed9d697e15c6a790e0aa549c36af38247462e2e3
catwithtudou <949812478@qq.com>
2025-01-05 13:41:38 +0800

    update: done the 2241

---
+++ go/leetcode/2241.go
@@ -0,0 +1,43 @@
+﻿package leetcode
+
+var atmDenominations = [...]int{20, 50, 100, 200, 500}
+
+const atmKinds = len(atmDenominations)
+
+type ATM [atmKinds]int
+
+func ConstructorATM() ATM {
+	return ATM{}
+}
+
+func (this *ATM) Deposit(banknotesCount []int) {
+	for i, count := range banknotesCount {
+		this[i] += count
+	}
+}
+
+func (this *ATM) Withdraw(amount int) []int {
+	ans := make([]int, atmKinds)
+
+	for i := atmKinds - 1; i >= 0; i-- {
+		ans[i] = min(amount/atmDenominations[i], this[i])
+		amount -= ans[i] * atmDenominations[i]
+	}
+
+	if amount > 0 {
+		return []int{-1}
+	}
+
+	for i, count := range ans {
+		this[i] -= count
+	}
+
+	return ans
+}
+
+/**
+ * Your ATM object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Deposit(banknotesCount);
+ * param_2 := obj.Withdraw(amount);
+ */
---
+++ rust/src/leetcode/two_thousand_two_hundred_forty_one.rs
@@ -0,0 +1,41 @@
+﻿pub struct Solution;
+
+struct ATM {
+    account: [i32; ATM::KINDS],
+}
+
+impl ATM {
+    const DENOMINATIONS: [i32; 5] = [20, 50, 100, 200, 500];
+    const KINDS: usize = Self::DENOMINATIONS.len();
+
+    fn new() -> Self {
+        Self {
+            account: [0; Self::KINDS],
+        }
+    }
+
+    fn deposit(&mut self, banknotes_count: Vec<i32>) {
+        for i in 0..Self::KINDS {
+            self.account[i] += banknotes_count[i];
+        }
+    }
+
+    fn withdraw(&mut self, mut amount: i32) -> Vec<i32> {
+        let mut ans = vec![0; Self::KINDS];
+
+        for i in (0..Self::KINDS).rev() {
+            ans[i] = self.account[i].min(amount / Self::DENOMINATIONS[i]);
+            amount -= ans[i] * Self::DENOMINATIONS[i];
+        }
+
+        if amount > 0 {
+            return vec![-1];
+        }
+
+        for i in 0..Self::KINDS {
+            self.account[i] -= ans[i];
+        }
+
+        ans
+    }
+}
