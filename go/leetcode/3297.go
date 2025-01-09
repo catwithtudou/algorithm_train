d856586e595c6b23d184598d905dbcb8e259d5ae
catwithtudou <949812478@qq.com>
2025-01-09 10:16:53 +0800

    update: done the 3297

---
+++ go/leetcode/3297.go
@@ -0,0 +1,38 @@
+﻿package leetcode
+
+func validSubstringCount(word1 string, word2 string) (ans int64) {
+	if len(word1) < len(word2) {
+		return 0
+	}
+
+	diff := [26]int{}
+	for _, c := range word2 {
+		diff[c-'a']++
+	}
+
+	less := 0
+	for _, c := range diff {
+		if c > 0 {
+			less++
+		}
+	}
+
+	left := 0
+	for _, c := range word1 {
+		diff[c-'a']--
+		if diff[c-'a'] == 0 {
+			less--
+		}
+		for less == 0 {
+			if diff[word1[left]-'a'] == 0 {
+				less++
+			}
+			diff[word1[left]-'a']++
+			left++
+		}
+
+		ans += int64(left)
+	}
+
+	return ans
+}
