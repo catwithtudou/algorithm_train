ccbabcfa65520012ec95aeb011e7e397356421d4
catwithtudou <949812478@qq.com>
2025-01-12 18:57:20 +0800

    update: done the 2275

---
+++ go/leetcode/2275.go
@@ -0,0 +1,19 @@
+﻿package leetcode
+
+import (
+	"math/bits"
+
+	"golang.org/x/exp/slices"
+)
+
+func largestCombination(candidates []int) (ans int) {
+	m := bits.Len(uint(slices.Max(candidates)))
+	for i := range m {
+		cnt := 0
+		for _, x := range candidates {
+			cnt += x >> i & 1
+		}
+		ans = max(ans, cnt)
+	}
+	return ans
+}
---
+++ rust/src/leetcode/two_thousand_two_hundred_seventy_five.rs
@@ -0,0 +1,22 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn largest_combination(candidates: Vec<i32>) -> i32 {
+        fn max_len(candidates: &Vec<i32>, k: i32) -> i32 {
+            let mut res = 0;
+            for &num in candidates.iter() {
+                if num & (1 << k) != 0 {
+                    res += 1;
+                }
+            }
+            res
+        }
+
+        let mut res = 0;
+        for i in 0..24 {
+            // 遍历二进制位
+            res = res.max(max_len(&candidates, i));
+        }
+        res
+    }
+}
