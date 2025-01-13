1fad24f14462e44424451bcc9e223915ae419228
catwithtudou <949812478@qq.com>
2025-01-13 10:51:02 +0800

    update: done the 2270

---
+++ go/leetcode/2270.go
@@ -0,0 +1,17 @@
+﻿package leetcode
+
+func waysToSplitArray(nums []int) (ans int) {
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+
+	s := 0
+	for i := 0; i < len(nums)-1; i++ {
+		s += nums[i]
+		if s*2 >= total {
+			ans++
+		}
+	}
+	return
+}
---
+++ rust/src/leetcode/two_thousand_two_hundred_and_seventy.rs
@@ -0,0 +1,16 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn ways_to_split_array(nums: Vec<i32>) -> i32 {
+        let mut total = nums.iter().map(|&x| x as i64).sum();
+        let mut ans = 0;
+        let mut s = 0;
+        for i in 0..nums.len() - 1 {
+            s += nums[i] as i64;
+            if s * 2 >= total {
+                ans += 1;
+            }
+        }
+        ans
+    }
+}
