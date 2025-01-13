c2436e96561de73e6a69c250453837561ff20a79
catwithtudou <949812478@qq.com>
2025-01-13 10:52:01 +0800

    update: update the total

--- rust/src/leetcode/two_thousand_two_hundred_and_seventy.rs
+++ rust/src/leetcode/two_thousand_two_hundred_and_seventy.rs
@@ -2,7 +2,7 @@
 
 impl Solution {
     pub fn ways_to_split_array(nums: Vec<i32>) -> i32 {
-        let mut total = nums.iter().map(|&x| x as i64).sum();
+        let total = nums.iter().map(|&x| x as i64).sum();
         let mut ans = 0;
         let mut s = 0;
         for i in 0..nums.len() - 1 {
