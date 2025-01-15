e406ea58bfc6d8aeb8c0102500e2c9e1e502718d
catwithtudou <949812478@qq.com>
2025-01-15 09:11:39 +0800

    update: done the 3066

---
+++ go/leetcode/3066.go
@@ -0,0 +1,23 @@
+﻿package leetcode
+
+import (
+	"container/heap"
+	"sort"
+)
+
+func minOperations3066(nums []int, k int) (ans int) {
+	h := &hp3066{nums}
+	heap.Init(h)
+	for h.IntSlice[0] < k {
+		x := heap.Pop(h).(int)
+		h.IntSlice[0] += x * 2
+		heap.Fix(h, 0)
+		ans++
+	}
+	return
+}
+
+type hp3066 struct{ sort.IntSlice }
+
+func (hp3066) Push(any)    {}
+func (h *hp3066) Pop() any { a := h.IntSlice; v := a[len(a)-1]; h.IntSlice = a[:len(a)-1]; return v }
---
+++ rust/src/leetcode/three_thousand_and_sixty_six.rs
@@ -0,0 +1,19 @@
+﻿use std::collections::BinaryHeap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
+        let mut h = BinaryHeap::new();
+        for x in nums {
+            h.push(-x as i64);
+        }
+        let mut ans = 0;
+        while -h.peek().unwrap() < k as i64 {
+            let (x, y) = (h.pop().unwrap(), h.pop().unwrap());
+            h.push((x * 2 + y) as i64);
+            ans += 1;
+        }
+        ans
+    }
+}
