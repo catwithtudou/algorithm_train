9c4fbb7145d7f0afccfb261fbc50649f69419e43
catwithtudou <949812478@qq.com>
2025-01-20 19:31:06 +0800

    update: done the 2239

---
+++ go/leetcode/2239.go
@@ -0,0 +1,18 @@
+﻿package leetcode
+
+func findClosestNumber(nums []int) int {
+	dis, idx := -1, 0
+	for i, x := range nums {
+		if dis == -1 || dis > abs(x) {
+			dis = abs(x)
+			idx = i
+		} else if dis == abs(x) {
+			if nums[idx] < x {
+				idx = i
+			}
+		}
+
+	}
+
+	return nums[idx]
+}
---
+++ rust/src/leetcode/two_thousand_two_hundred_thirty_nine.rs
@@ -0,0 +1,7 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn find_closest_number(nums: Vec<i32>) -> i32 {
+        *nums.iter().min_by_key(|&x| (x.abs(), x)).unwrap()
+    }
+}
