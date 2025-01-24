c791a66ae3673d7a12aa5121d6b76cb5617f8c41
catwithtudou <949812478@qq.com>
2025-01-24 09:12:11 +0800

    update: done the 2944

---
+++ go/leetcode/2944.go
@@ -0,0 +1,26 @@
+﻿package leetcode
+
+import "math"
+
+func minimumCoins(prices []int) int {
+	n := len(prices)
+	memo := make([]int, (n+1)/2)
+	var dfs func(int) int
+	dfs = func(i int) (res int) {
+		if 2*i >= n {
+			return prices[i-1]
+		}
+		p := &memo[i]
+		if *p > 0 {
+			return *p
+		}
+		defer func() { *p = res }()
+
+		res = math.MaxInt
+		for j := i + 1; j <= 2*i+1; j++ {
+			res = min(res, dfs(j))
+		}
+		return res + prices[i-1]
+	}
+	return dfs(1)
+}
---
+++ rust/src/leetcode/two_thousand_nine_hundred_forty_four.rs
@@ -0,0 +1,14 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn minimum_coins(mut prices: Vec<i32>) -> i32 {
+        let n = prices.len();
+        for i in (1..=(n + 1) / 2 - 1).rev() {
+            let min_val = *prices[i..=2*i].into_iter().min().unwrap() as i32;
+            prices[i - 1] += min_val;
+        }
+
+        prices[0]
+    }
+}
+
