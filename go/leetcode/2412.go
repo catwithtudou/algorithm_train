79a62d30f883b222684c31059b24b084d1917011
catwithtudou <949812478@qq.com>
2025-01-25 22:11:27 +0800

    update: update the 2412

---
+++ go/leetcode/2412.go
@@ -0,0 +1,10 @@
+﻿package leetcode
+
+func minimumMoney(transactions [][]int) int64 {
+	totalLose, mx := 0, 0
+	for _, t := range transactions {
+		totalLose += max(t[0]-t[1], 0)
+		mx = max(mx, min(t[0], t[1]))
+	}
+	return int64(totalLose + mx)
+}
