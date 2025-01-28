a1a4b900a7dbe0e72ca6a5ae97c7b6aa4fc51ca1
catwithtudou <949812478@qq.com>
2025-01-28 11:43:22 +0800

    upadte: done the 119

---
+++ go/leetcode/119.go
@@ -0,0 +1,13 @@
+﻿package leetcode
+
+func getRow(rowIndex int) []int {
+	var c [34][]int
+	for i := range c {
+		c[i] = make([]int, i+1)
+		c[i][0], c[i][i] = 1, 1
+		for j := 1; j < i; j++ {
+			c[i][j] = c[i-1][j-1] + c[i-1][j]
+		}
+	}
+	return c[rowIndex]
+}
---
+++ rust/src/leetcode/one_hundred_and_nineteen.rs
@@ -0,0 +1,30 @@
+﻿pub struct Solution;
+
+static mut INITIALIZED: bool = false;
+static mut C: [[i32; 34]; 34] = [[0; 34]; 34];
+
+unsafe fn init_once() {
+    if INITIALIZED {
+        return;
+    }
+
+    INITIALIZED = true;
+
+    for i in 0..C.len() {
+        C[i][0] = 1;
+        C[i][i] = 1;
+        for j in 1..i {
+            C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+        }
+    }
+}
+
+impl Solution {
+    pub fn get_row(row_index: i32) -> Vec<i32> {
+        let row_index = row_index as usize;
+        unsafe {
+            init_once();
+            C[row_index][..=row_index].to_vec()
+        }
+    }
+}
