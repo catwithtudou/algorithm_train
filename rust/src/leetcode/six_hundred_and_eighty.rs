da2d0880dc4b27b9cd1af9400458fdc658a96701
catwithtudou <949812478@qq.com>
2025-02-03 19:55:13 +0800

    update: update the 680

---
+++ rust/src/leetcode/six_hundred_and_eighty.rs
@@ -0,0 +1,27 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn valid_palindrome(s: String) -> bool {
+        fn check(mut i: usize, mut j: usize, s: &[u8]) -> bool {
+            while i < j {
+                if s[i] != s[j] {
+                    return false;
+                }
+                i += 1;
+                j -= 1;
+            }
+            return true;
+        }
+
+        let s = s.as_bytes();
+        let (mut i, mut j) = (0, s.len() - 1);
+        while i < j {
+            if s[i] != s[j] {
+                return check(i + 1, j, s) || check(i, j - 1, s);
+            }
+            i += 1;
+            j -= 1;
+        }
+        return true;
+    }
+}
