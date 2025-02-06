9f17ff65d21fd50f694ef68da2ec4b278c01ffd7
catwithtudou <949812478@qq.com>
2025-02-06 11:35:44 +0800

    update: done the 598

---
+++ go/leetcode/598.go
@@ -0,0 +1,11 @@
+﻿package leetcode
+
+func maxCount(m int, n int, ops [][]int) int {
+	minM, minN := m, n
+	for _, op := range ops {
+		minM = min(minM, op[0])
+		minN = min(minN, op[1])
+	}
+
+	return minM * minN
+}
---
+++ rust/src/leetcode/five_hundred_and_ninety_eight.rs
@@ -0,0 +1,11 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn max_count(mut m: i32, mut n: i32, ops: Vec<Vec<i32>>) -> i32 {
+        for op in ops {
+            m = m.min(op[0]);
+            n = n.min(op[1]);
+        }
+        m * n
+    }
+}
