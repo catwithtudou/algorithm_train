ce7aa7a7082f05d0a96d7d281ca1d4ed54a97aa9
catwithtudou <949812478@qq.com>
2025-02-12 19:40:50 +0800

    update: done the 1760

---
+++ go/leetcode/1760.go
@@ -0,0 +1,19 @@
+﻿package leetcode
+
+import (
+	"slices"
+	"sort"
+)
+
+func minimumSize(nums []int, maxOperations int) int {
+	left, right := 1, slices.Max(nums)
+	return left + sort.Search(right-left, func(m int) bool {
+		m += left
+		cnt := 0
+		for _, x := range nums {
+			cnt += (x - 1) / m
+		}
+
+		return cnt <= maxOperations
+	})
+}
---
+++ rust/src/leetcode/one_thousand_seven_hundred_and_sixty.rs
@@ -0,0 +1,24 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn minimum_size(nums: Vec<i32>, max_operations: i32) -> i32 {
+        let check = |m: i32| -> bool {
+            let mut cnt = 0;
+            for i in 0..nums.len() {
+                cnt += ((nums[i] - 1) / m) as i64;
+            }
+            cnt <= max_operations as i64
+        };
+        let mut left = 0;
+        let mut right = *nums.iter().max().unwrap();
+        while left + 1 < right {
+            let mid = left + (right - left) / 2;
+            if check(mid) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        right
+    }
+}
