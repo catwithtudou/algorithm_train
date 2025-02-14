d87279a2deab59ffa80486cb9f0d6f8a4900b4fe
catwithtudou <949812478@qq.com>
2025-02-14 09:29:57 +0800

    update: done the 1552

---
+++ go/leetcode/1552.go
@@ -0,0 +1,23 @@
+﻿package leetcode
+
+import (
+	"slices"
+	"sort"
+)
+
+func maxDistance(position []int, m int) int {
+	slices.Sort(position)
+	return sort.Search((position[len(position)-1]-position[0])/(m-1), func(d int) bool {
+		d++
+		cnt, pre := 1, position[0]
+		for _, x := range position[1:] {
+			if x >= d+pre {
+				cnt++
+				pre = x
+			}
+		}
+
+		return cnt < m
+	})
+
+}
---
+++ rust/src/leetcode/one_thousand_five_hundred_fifty_two.rs
@@ -0,0 +1,31 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn max_distance(mut position: Vec<i32>, m: i32) -> i32 {
+        position.sort_unstable();
+
+        let check = |d: i32| -> i32 {
+            let mut cnt = 1;
+            let mut pre = position[0];
+            for &x in &position {
+                if x >= pre + d {
+                    cnt += 1;
+                    pre = x;
+                }
+            }
+            cnt
+        };
+
+        let mut left = 0;
+        let mut right = (position.last().unwrap() - position[0]) / (m - 1) + 1;
+        while left + 1 < right {
+            let mid = left + (right - left) / 2;
+            if check(mid) >= m {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        left
+    }
+}
