c9c9d1943493d82477efc352543df7430ae247fe
catwithtudou <949812478@qq.com>
2025-02-16 10:21:43 +0800

    update: done the 1299

---
+++ go/leetcode/1299.go
@@ -0,0 +1,13 @@
+﻿package leetcode
+
+func replaceElements(arr []int) []int {
+	n := len(arr)
+	pre := -1
+	for i := n - 1; i >= 0; i-- {
+		temp := arr[i]
+		arr[i] = pre
+		pre = max(pre, temp)
+	}
+
+	return arr
+}
---
+++ rust/src/leetcode/one_thousand_two_hundred_ninety_nine.rs
@@ -0,0 +1,13 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn replace_elements(mut arr: Vec<i32>) -> Vec<i32> {
+        let mut pre = -1;
+        for i in (0..arr.len()).rev() {
+            let cur = arr[i];
+            arr[i] = pre;
+            pre = pre.max(cur);
+        }
+        arr
+    }
+}
