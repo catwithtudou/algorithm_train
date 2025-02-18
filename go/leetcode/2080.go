45cd6b376318ce23e204555ffb35678baf1530fe
catwithtudou <949812478@qq.com>
2025-02-18 09:38:18 +0800

    update: done the 2080

---
+++ go/leetcode/2080.go
@@ -0,0 +1,24 @@
+﻿package leetcode
+
+import "sort"
+
+type RangeFreqQuery map[int][]int
+
+func ConstructorRangeFreqQuery(arr []int) RangeFreqQuery {
+	pos := map[int][]int{}
+	for i, x := range arr {
+		pos[x] = append(pos[x], i)
+	}
+	return pos
+}
+
+func (this RangeFreqQuery) Query(left int, right int, value int) int {
+	a := this[value]
+	return sort.SearchInts(a, right+1) - sort.SearchInts(a, left)
+}
+
+/**
+ * Your RangeFreqQuery object will be instantiated and called as such:
+ * obj := Constructor(arr);
+ * param_1 := obj.Query(left,right,value);
+ */
---
+++ rust/src/leetcode/two_thousand_and_eighty.rs
@@ -0,0 +1,33 @@
+﻿use std::collections::HashMap;
+
+pub struct Solution;
+
+struct RangeFreqQuery {
+    pos:HashMap<i32,Vec<usize>>,
+}
+
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl RangeFreqQuery {
+
+    fn new(arr: Vec<i32>) -> Self {
+        let mut pos = HashMap::new();
+        for (i,&x) in arr.iter().enumerate() {
+            pos.entry(x).or_insert(vec![]).push(i);
+        }
+        Self{pos}
+    }
+
+    fn query(&self, left: i32, right: i32, value: i32) -> i32 {
+        if let Some(a) = self.pos.get(&value) {
+            let p = a.partition_point(|&i| i< left as usize);
+            let q = a.partition_point(|&i| i<= right as usize);
+            (q-p) as _
+        }else{
+            0
+        }
+    }
+}
