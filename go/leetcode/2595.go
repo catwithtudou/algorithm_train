e4d158adcc155f12301d9c88c8b494f83565f67a
catwithtudou <949812478@qq.com>
2025-02-20 09:37:14 +0800

    update: done the 2595

---
+++ go/leetcode/2595.go
@@ -0,0 +1,10 @@
+﻿package leetcode
+
+func evenOddBit(n int) []int {
+	ans := make([]int, 2)
+	for i := 0; n > 0; n >>= 1 {
+		ans[i] += n & 1
+		i ^= 1
+	}
+	return ans
+}
---
+++ rust/src/leetcode/two_thousand_five_hundred_ninety_five.rs
@@ -0,0 +1,8 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn even_odd_bit(n: i32) -> Vec<i32> {
+        const MASK: i32 = 0x55555555;
+        vec![(n & MASK).count_ones() as i32, (n & MASK << 1).count_ones() as i32]
+    }
+}
