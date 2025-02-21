2d653cd74ec4f13d651cc0adb4a4a404322e3d83
catwithtudou <949812478@qq.com>
2025-02-21 09:51:17 +0800

    update: done the 2209

---
+++ go/leetcode/2209.go
@@ -0,0 +1,33 @@
+﻿package leetcode
+
+func minimumWhiteTiles(floor string, numCarpets int, carpetLen int) int {
+	n := len(floor)
+	memo := make([][]int, numCarpets+1)
+	for i := range memo {
+		memo[i] = make([]int, n)
+		for j := range memo[i] {
+			memo[i][j] = -1
+		}
+	}
+
+	var dfs func(int, int) int
+	dfs = func(i, j int) (res int) {
+		if j < i*carpetLen {
+			return
+		}
+
+		p := &memo[i][j]
+		if *p != -1 {
+			return *p
+		}
+		defer func() { *p = res }()
+
+		res = dfs(i, j-1) + int(floor[j]-'0')
+		if i > 0 {
+			res = min(res, dfs(i-1, j-carpetLen))
+		}
+		return
+	}
+
+	return dfs(numCarpets, n-1)
+}
---
+++ rust/src/leetcode/two_thousand_two_hundred_and_nine.rs
@@ -0,0 +1,26 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn minimum_white_tiles(floor: String, num_carpets: i32, carpet_len: i32) -> i32 {
+        let n = floor.len();
+        let num_carpets = num_carpets as usize;
+        let carpet_len = carpet_len as usize;
+        let floor = floor.as_bytes();
+
+        let mut f = vec![vec![0; n]; num_carpets + 1];
+
+        f[0][0] = (floor[0] - b'0') as i32;
+        for j in 1..n {
+            f[0][j] = f[0][j - 1] + (floor[j] - b'0') as i32;
+        }
+
+        for i in 1..=num_carpets  {
+            for j in (i * carpet_len)..n {
+                f[i][j] =
+                    f[i - 1][j - carpet_len].min(f[i][j - 1] + (floor[j] - b'0') as i32);
+            }
+        }
+
+        f[num_carpets][n - 1]
+    }
+}
