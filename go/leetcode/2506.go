77299257b3e95ebe76730c5eb97cfb26ed6f4f64
catwithtudou <949812478@qq.com>
2025-02-22 10:28:15 +0800

    update: done the 2506

---
+++ go/leetcode/2506.go
@@ -0,0 +1,15 @@
+﻿package leetcode
+
+func similarPairs(words []string) (ans int) {
+	cnt := make(map[int]int)
+	for _, word := range words {
+		mask := 0
+		for _, c := range word {
+			mask |= 1 << (c - 'a')
+		}
+		ans += cnt[mask]
+		cnt[mask]++
+	}
+
+	return ans
+}
---
+++ rust/src/leetcode/two_thousand_five_hundred_and_six.rs
@@ -0,0 +1,21 @@
+﻿use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn similar_pairs(words: Vec<String>) -> i32 {
+        let mut ans = 0;
+        let mut cnt = HashMap::new();
+
+        for s in words {
+            let mut mask = 0;
+            for c in s.as_bytes() {
+                mask |= 1 << (c - b'a');
+            }
+            ans += *cnt.get(&mask).unwrap_or(&0);
+            *cnt.entry(mask).or_insert(0) += 1;
+        }
+
+        ans
+    }
+}
