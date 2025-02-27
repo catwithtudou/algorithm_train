babb31db5c9a7e68209b253a3e9668457b367c45
catwithtudou <949812478@qq.com>
2025-02-27 09:21:48 +0800

    update: done the 2296

---
+++ go/leetcode/2296.go
@@ -0,0 +1,51 @@
+﻿package leetcode
+
+type TextEditor struct {
+	left, right []byte
+}
+
+func ConstructorTextEditor() TextEditor {
+	return TextEditor{}
+}
+
+func (this *TextEditor) AddText(text string) {
+	this.left = append(this.left, text...)
+	return
+}
+
+func (this *TextEditor) text() string {
+	return string(this.left[max(0, len(this.left)-10):len(this.left)])
+}
+
+func (this *TextEditor) DeleteText(k int) int {
+	k = min(len(this.left), k)
+	this.left = this.left[:len(this.left)-k]
+	return k
+}
+
+func (this *TextEditor) CursorLeft(k int) string {
+	for k > 0 && len(this.right) > 0 {
+		this.right = append(this.right, this.left[len(this.left)-1])
+		this.left = this.left[:len(this.left)-1]
+		k--
+	}
+	return this.text()
+}
+
+func (this *TextEditor) CursorRight(k int) string {
+	for k > 0 && len(this.left) > 0 {
+		this.left = append(this.left, this.right[len(this.right)-1])
+		this.right = this.right[:len(this.right)-1]
+		k--
+	}
+	return this.text()
+}
+
+/**
+ * Your TextEditor object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AddText(text);
+ * param_2 := obj.DeleteText(k);
+ * param_3 := obj.CursorLeft(k);
+ * param_4 := obj.CursorRight(k);
+ */
---
+++ rust/src/leetcode/two_thousand_two_hundred_ninety_six.rs
@@ -0,0 +1,50 @@
+﻿#[allow(dead_code)]
+pub struct TextEditor {
+    left: Vec<u8>,  // 光标左侧字符
+    right: Vec<u8>, // 光标右侧字符
+}
+
+#[allow(dead_code)]
+impl TextEditor {
+    fn new() -> Self {
+        TextEditor {
+            left: Vec::new(),
+            right: Vec::new(),
+        }
+    }
+
+    fn add_text(&mut self, text: String) {
+        self.left.extend(text.as_bytes());
+    }
+
+    fn delete_text(&mut self, k: i32) -> i32 {
+        let delete_count = k.min(self.left.len() as i32);
+        self.left.truncate(self.left.len() - delete_count as usize);
+        delete_count
+    }
+
+    fn text(&self) -> String {
+        let start = (self.left.len() as i32 - 10).max(0) as usize;
+        String::from_utf8(self.left[start..].to_vec()).unwrap()
+    }
+
+    fn cursor_left(&mut self, mut k: i32) -> String {
+        while k > 0 && !self.left.is_empty() {
+            if let Some(byte) = self.left.pop() {
+                self.right.push(byte);
+            }
+            k -= 1;
+        }
+        self.text()
+    }
+
+    fn cursor_right(&mut self,mut k: i32) -> String {
+        while k > 0 && !self.right.is_empty() {
+            if let Some(byte) = self.right.pop() {
+                self.left.push(byte);
+            }
+            k -= 1;
+        }
+        self.text()
+    }
+}
