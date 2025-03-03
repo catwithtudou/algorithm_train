49cc54e4d9de80ed9431757b3140f273fd4f015c
catwithtudou <949812478@qq.com>
2025-03-03 10:27:50 +0800

    update: done the 1278

---
+++ go/leetcode/1278.go
@@ -0,0 +1,64 @@
+﻿package leetcode
+
+import "math"
+
+func palindromePartition(s string, k int) int {
+	n := len(s)
+
+	changeMemo := make([][]int, n)
+	for i := range changeMemo {
+		changeMemo[i] = make([]int, n)
+		for j := range changeMemo[i] {
+			changeMemo[i][j] = -1
+		}
+	}
+
+	var change func(int, int) int
+	change = func(l, r int) (res int) {
+		if l >= r {
+			return
+		}
+		p := &changeMemo[l][r]
+		if *p != -1 {
+			return *p
+		}
+		defer func() { *p = res }()
+
+		res = change(l+1, r-1)
+		if s[l] != s[r] {
+			res++
+		}
+
+		return
+	}
+
+	memo := make([][]int, k)
+	for i := range memo {
+		memo[i] = make([]int, n)
+		for j := range memo[i] {
+			memo[i][j] = -1
+		}
+	}
+
+	var dfs func(int, int) int
+	dfs = func(i, r int) (res int) {
+		if i == 0 {
+			return change(0, r)
+		}
+
+		p := &memo[i][r]
+		if *p != -1 {
+			return *p
+		}
+		defer func() { *p = res }()
+
+		res = math.MaxInt
+		for l := i; l <= r; l++ {
+			res = min(res, dfs(i-1, l-1)+change(l, r))
+		}
+
+		return
+	}
+
+	return dfs(k-1, n-1)
+}
---
+++ rust/src/leetcode/one_thousand_two_hundred_seventy_eight.rs
@@ -0,0 +1,31 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn palindrome_partition(s: String, k: i32) -> i32 {
+        let s_bytes = s.as_bytes();
+        let n = s_bytes.len();
+        let k = k as usize;
+
+        let mut change = vec![vec![0; n]; n];
+        for i in (0..=n - 1).rev() {
+            for j in (i + 1)..n {
+                change[i][j] = change[i + 1][j - 1];
+                if s_bytes[i] != s_bytes[j] {
+                    change[i][j] += 1;
+                }
+            }
+        }
+
+        let mut f = vec![vec![0; n]; k];
+        f[0] = change[0].clone();
+        for i in 1..k {
+            for r in i..=(n - k + i) {
+                f[i][r] = i32::MAX;
+                for l in i..=r {
+                    f[i][r] = f[i][r].min(f[i - 1][l - 1] + change[l][r]);
+                }
+            }
+        }
+        f[k - 1][n - 1]
+    }
+}
