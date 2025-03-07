2cfa7a1765cd20f331da3ff297ea1a5234eb1abd
catwithtudou <949812478@qq.com>
2025-03-07 09:13:52 +0800

    update: done the 2597

---
+++ go/leetcode/2597.go
@@ -0,0 +1,42 @@
+﻿package leetcode
+
+import (
+	"sort"
+)
+
+func beautifulSubsets(nums []int, k int) (ans int) {
+	group := make(map[int]map[int]int)
+	for _, x := range nums {
+		if group[x%k] == nil {
+			group[x%k] = make(map[int]int)
+		}
+		group[x%k][x]++
+	}
+
+	ans = 1
+
+	for _, cnt := range group {
+		a := []int{}
+		for x := range cnt {
+			a = append(a, x)
+		}
+		sort.Ints(a)
+
+		m := len(a)
+		f := make([]int, m+1)
+		f[0] = 1
+		f[1] = 1 << cnt[a[0]]
+		for i := 1; i < m; i++ {
+			c := cnt[a[i]]
+			if a[i]-a[i-1] == k {
+				f[i+1] = f[i-1]*(1<<c-1) + f[i]
+			} else {
+				f[i+1] = f[i] << c
+			}
+		}
+
+		ans *= f[m]
+	}
+
+	return ans - 1
+}
---
+++ rust/src/leetcode/two_thousand_five_hundred_ninety_seven.rs
@@ -0,0 +1,32 @@
+﻿pub struct Solution;
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn beautiful_subsets(nums: Vec<i32>, k: i32) -> i32 {
+        let mut ans = -1;
+        let mut cnt: HashMap<i32, i32> = HashMap::new();
+
+        fn dfs(i: usize, nums: &Vec<i32>, k: i32, cnt: &mut HashMap<i32, i32>, ans: &mut i32) {
+            if i == nums.len() {
+                *ans += 1;
+                return;
+            }
+
+            // 不选
+            dfs(i + 1, nums, k, cnt, ans);
+
+            // 选
+            let x = nums[i];
+            if *cnt.get(&(x - k)).unwrap_or(&0) == 0 && *cnt.get(&(x + k)).unwrap_or(&0) == 0 {
+                *cnt.entry(x).or_insert(0) += 1;
+                dfs(i + 1, nums, k, cnt, ans);
+                *cnt.get_mut(&x).unwrap() -= 1;
+            }
+        }
+
+        dfs(0,&nums, k,&mut cnt,&mut ans);
+
+        ans
+    }
+}
