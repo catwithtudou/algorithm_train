f311dd8432036ba384047d48efb6b20242086aa4
catwithtudou <949812478@qq.com>
2025-03-12 09:21:41 +0800

    update: done the 3305

---
+++ go/leetcode/3305.go
@@ -0,0 +1,39 @@
+﻿package leetcode
+
+import "strings"
+
+func countOfSubstrings(word string, k int) int {
+	return countF(word, k) - countF(word, k+1)
+}
+
+func countF(word string, k int) (ans int) {
+
+	cnt1 := map[byte]int{}
+	cnt2, left := 0, 0
+
+	for _, b := range word {
+
+		if strings.ContainsRune("aeiou", b) {
+			cnt1[byte(b)]++
+		} else {
+			cnt2++
+		}
+
+		for len(cnt1) == 5 && cnt2 >= k {
+			out := word[left]
+			if strings.ContainsRune("aeiou", rune(out)) {
+				cnt1[out]--
+				if cnt1[out] == 0 {
+					delete(cnt1, out)
+				}
+			} else {
+				cnt2--
+			}
+			left++
+		}
+
+		ans += left
+	}
+
+	return ans
+}
---
+++ rust/src/leetcode/three_thousand_three_hundred_and_five.rs
@@ -0,0 +1,49 @@
+﻿pub struct Solution;
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn count_of_substrings(word: String, k: i32) -> i32 {
+        Self::count_f(&word, k) - Self::count_f(&word, k + 1)
+    }
+
+    fn count_f(word: &str, k: i32) -> i32 {
+        let mut count = 0;
+        let mut vowel_counts = HashMap::new();
+        let mut non_vowel_count = 0;
+        let mut left = 0;
+        let bytes = word.as_bytes();
+
+        for right in 0..bytes.len() {
+            match bytes[right] {
+                b'a' | b'e' | b'i' | b'o' | b'u' => {
+                    *vowel_counts.entry(bytes[right]).or_insert(0) += 1;
+                }
+                _ => {
+                    non_vowel_count += 1;
+                }
+            }
+
+            while vowel_counts.len() == 5 && non_vowel_count >= k {
+                match bytes[left] {
+                    b'a' | b'e' | b'i' | b'o' | b'u' => {
+                        if let Some(val) = vowel_counts.get_mut(&bytes[left]) {
+                            *val -= 1;
+                            if *val == 0 {
+                                vowel_counts.remove(&bytes[left]);
+                            }
+                        }
+                    }
+                    _ => {
+                        non_vowel_count -= 1;
+                    }
+                }
+                left += 1;
+            }
+
+            count += left as i32;
+        }
+
+        count
+    }
+}
