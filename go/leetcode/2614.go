4f04b227613e84ff11b5ac43e741b96b37a97cb1
catwithtudou <949812478@qq.com>
2025-03-18 09:41:11 +0800

    upadte: done the 2614

---
+++ go/leetcode/2614.go
@@ -0,0 +1,15 @@
+﻿package leetcode
+
+func diagonalPrime(nums [][]int) (ans int) {
+	n := len(nums)
+	for i, row := range nums {
+		if num := row[i]; num > ans && isPrime(num) {
+			ans = num
+		}
+		if num := row[n-i-1]; num > ans && isPrime(num) {
+			ans = num
+		}
+	}
+
+	return
+}
---
+++ rust/src/leetcode/two_thousand_six_hundred_and_forteen.rs
@@ -0,0 +1,28 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn diagonal_prime(nums: Vec<Vec<i32>>) -> i32 {
+        let mut ans = 0;
+
+        let n = nums.len();
+
+        for (i, row) in nums.iter().enumerate() {
+            for x in [row[i], row[n - 1 - i]] {
+                if x>ans && Self::is_prime(x) {
+                    ans = x;
+                }
+            }
+        }
+
+        ans
+    }
+
+    fn is_prime(n: i32) -> bool {
+        for i in 2..=((n as f64).sqrt() as i32) {
+            if n % i == 0 {
+                return false;
+            }
+        }
+        n >= 2
+    }
+}
