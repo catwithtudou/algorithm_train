955eb73563253db78b522bb3bbef9413023d8947
catwithtudou <949812478@qq.com>
2025-03-25 09:44:23 +0800

    update: done the 2711

---
+++ go/leetcode/2771.go
@@ -0,0 +1,30 @@
+﻿package leetcode
+
+func differenceOfDistinctValues(grid [][]int) [][]int {
+	m, n := len(grid), len(grid[0])
+	ans := make([][]int, m)
+	set := make(map[int]struct{})
+
+	for i := range m {
+		ans[i] = make([]int, n)
+		for j := range n {
+			clear(set)
+
+			for x, y := i-1, j-1; x >= 0 && y >= 0; x, y = x-1, y-1 {
+				set[grid[x][y]] = struct{}{}
+			}
+			topLeft := len(set)
+
+			clear(set)
+
+			for x, y := i+1, j+1; x < m && y < n; x, y = x+1, y+1 {
+				set[grid[x][y]] = struct{}{}
+			}
+			bottomRight := len(set)
+
+			ans[i][j] = abs(topLeft - bottomRight)
+		}
+	}
+
+	return ans
+}
---
+++ rust/src/leetcode/two_thousand_seven_hundred_and_eleven.rs
@@ -0,0 +1,31 @@
+﻿use std::collections::HashSet;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn difference_of_distinct_values(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let (m, n) = (grid.len(), grid[0].len());
+        let mut ans = vec![vec![0; n]; m];
+
+        for k in 1..(m + n) {
+            let min_j = (n as i32 - k as i32).max(0) as usize;
+            let max_j = ((m + n - 1) as i32 - k as i32).min(n as i32 - 1) as usize;
+
+            let mut set = HashSet::new();
+            for j in min_j..=max_j {
+                let i = k + j - n;
+                ans[i][j] = set.len() as i32;
+                set.insert(grid[i][j]);
+            }
+
+            let mut set = HashSet::new();
+            for j in (min_j..=max_j).rev() {
+                let i = k + j - n;
+                ans[i][j] = (ans[i][j] - set.len() as i32).abs();
+                set.insert(grid[i][j]);
+            }
+        }
+
+        ans
+    }
+}
