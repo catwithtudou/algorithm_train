fb0a40a114b92a49149fce122c550f16849952b1
catwithtudou <949812478@qq.com>
2025-03-26 09:02:49 +0800

    update: done the 2829

---
+++ go/leetcode/2829.go
@@ -0,0 +1,22 @@
+﻿package leetcode
+
+func minimumSumKAvoidng(n int, k int) (ans int) {
+	limitNum := make(map[int]struct{})
+	for i, cnt := 1, 0; cnt < n; i++ {
+		if _, ok := limitNum[i]; ok {
+			continue
+		}
+
+		if i >= k {
+			ans += i
+			cnt++
+			continue
+		}
+
+		limitNum[k-i] = struct{}{}
+		ans += i
+		cnt++
+	}
+
+	return
+}
---
+++ rust/src/leetcode/two_thousand_eight_hundred_twenty_nine.rs
@@ -0,0 +1,8 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn minimum_sum(n: i32, k: i32) -> i32 {
+        let m = n.min(k / 2);
+        (m * (m + 1) + (k * 2 + n - m - 1) * (n - m)) / 2
+    }
+}
