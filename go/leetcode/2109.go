d0ea2b23e06de3d7ce7f3749e500d800951728c4
catwithtudou <949812478@qq.com>
2025-03-30 11:04:43 +0800

    update: update the 2109

---
+++ go/leetcode/2109.go
@@ -0,0 +1,17 @@
+﻿package leetcode
+
+func addSpaces(s string, spaces []int) string {
+	ans := make([]byte, 0, len(s)+len(spaces))
+
+	j := 0
+
+	for i, c := range s {
+		if j < len(spaces) && i == spaces[j] {
+			ans = append(ans, ' ')
+			j++
+		}
+		ans = append(ans, byte(c))
+	}
+
+	return string(ans)
+}
---
+++ rust/src/leetcode/two_thousand_one_hundred_and_nine.rs
@@ -0,0 +1,19 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn add_spaces(s: String, spaces: Vec<i32>) -> String {
+        let mut spaces_copy = spaces.clone();
+        spaces_copy.push(s.len() as i32);
+
+        let mut ans = String::with_capacity(s.len() + spaces.len());
+
+        ans.push_str(&s[0..spaces_copy[0] as usize]);
+
+        for i in 1..spaces_copy.len() {
+            ans.push(' ');
+            ans.push_str(&s[spaces_copy[i-1] as usize..spaces_copy[i] as usize]);
+        }
+
+        ans
+    }
+}
