cdb4001eee9a23a21cb58148afa116a82974167a
catwithtudou <949812478@qq.com>
2025-04-01 10:43:13 +0800

    update: done the 2140

---
+++ go/leetcode/2140.go
@@ -0,0 +1,20 @@
+﻿package leetcode
+
+func mostPoints(questions [][]int) int64 {
+	n := len(questions)
+	memo := make([]int64, n)
+	var dfs func(int) int64
+	dfs = func(i int) int64 {
+		if i >= n {
+			return 0
+		}
+		p := &memo[i]
+		if *p == 0 {
+			q := questions[i]
+			*p = maxInt64(dfs(i+1), dfs(i+q[1]+1)+int64(q[0]))
+		}
+		return *p
+	}
+
+	return dfs(0)
+}
---
+++ rust/src/leetcode/two_thousand_one_hundred_and_forty.rs
@@ -0,0 +1,14 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn most_points(questions: Vec<Vec<i32>>) -> i64 {
+        let n = questions.len();
+        let mut f = vec![0 as i64; n + 1];
+
+        for i in (0..n).rev() {
+            f[i] = f[i+1].max(f[n.min(i+questions[i][1] as usize+1)]+questions[i][0] as i64);
+        }
+
+        f[0]
+    }
+}
