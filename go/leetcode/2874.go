8edc0c3892b0a73181cce405eaefa15b6830535e
catwithtudou <949812478@qq.com>
2025-04-03 10:00:03 +0800

    update: done the 2874

---
+++ go/leetcode/2874.go
@@ -0,0 +1,12 @@
+﻿package leetcode
+
+func maximumTripletValueII(nums []int) int64 {
+	ans := 0
+	preMax, maxDiff := 0, 0
+	for _, x := range nums {
+		ans = max(ans, maxDiff*x)
+		maxDiff = max(maxDiff, preMax-x)
+		preMax = max(preMax, x)
+	}
+	return int64(ans)
+}
---
+++ rust/src/leetcode/two_thousand_eight_hundred_seventy_four.rs
@@ -0,0 +1,19 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn maximum_triplet_value(nums: Vec<i32>) -> i64 {
+        let mut ans = 0;
+        let n = nums.len();
+        let mut suf_max = vec![0; n + 1];
+        for i in (0..n).rev() {
+            suf_max[i] = suf_max[i + 1].max(nums[i]);
+        }
+
+        let mut pre_max = 0;
+        for i in 0..n {
+            ans = ans.max((pre_max-nums[i]) as i64 * (suf_max[i + 1] as i64));
+            pre_max = pre_max.max(nums[i]);
+        }
+        ans
+    }
+}
