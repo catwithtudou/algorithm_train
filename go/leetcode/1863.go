5a7e7919e9c89c24067d1f93d0a6b01b12b62aaa
catwithtudou <949812478@qq.com>
2025-04-05 12:10:23 +0800

    updater: done the 1863

---
+++ go/leetcode/1863.go
@@ -0,0 +1,20 @@
+﻿package leetcode
+
+func subsetXORSum(nums []int) int {
+	var dfs func(int, int) int
+	dfs = func(idx int, xor int) int {
+		if idx == len(nums) {
+			return xor
+		}
+		return dfs(idx+1, xor^nums[idx]) + dfs(idx+1, xor)
+	}
+	return dfs(0, 0)
+}
+
+func subsetXORSumII(nums []int) int {
+	or := 0
+	for _, x := range nums {
+		or |= x
+	}
+	return or << (len(nums) - 1)
+}
---
+++ rust/src/leetcode/one_thousand_eight_hundred_sixty_three.rs
@@ -0,0 +1,8 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn subset_xor_sum(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        nums.into_iter().reduce(|or, x| or | x).unwrap() << (n - 1)
+    }
+}
