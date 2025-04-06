77e62aca3994314224a1b50811929ea7513b015b
catwithtudou <949812478@qq.com>
2025-04-06 10:42:31 +0800

    update: done the 368

---
+++ go/leetcode/368.go
@@ -0,0 +1,51 @@
+﻿package leetcode
+
+import "sort"
+
+func largestDivisibleSubset(nums []int) []int {
+	n := len(nums)
+	from := make([]int, n)
+	for i := range from {
+		from[i] = -1
+	}
+
+	sort.Ints(nums)
+	memo := make([]int, n)
+	var dfs func(int) int
+	dfs = func(i int) (res int) {
+		p := &memo[i]
+		if *p > 0 {
+			return *p
+		}
+		defer func() { *p = res }()
+
+		x := nums[i]
+		for j, v := range nums[:i] {
+			if x%v != 0 {
+				continue
+			}
+			f := dfs(j)
+			if f > res {
+				res = f
+				from[i] = j
+			}
+		}
+		return res + 1
+	}
+
+	maxF, maxI := 0, 0
+	for i := range n {
+		f := dfs(i)
+		if f > maxF {
+			maxF = f
+			maxI = i
+		}
+	}
+
+	path := make([]int, 0, maxF)
+	for i := maxI; i >= 0; i = from[i] {
+		path = append(path, nums[i])
+	}
+
+	return path
+}
---
+++ rust/src/leetcode/three_hundred_sixty_and_eight.rs
@@ -0,0 +1,33 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn largest_divisible_subset(mut nums: Vec<i32>) -> Vec<i32> {
+        let n = nums.len();
+        let mut from = vec![-1; n];
+        let mut f = vec![0; n];
+        nums.sort();
+
+        let mut max_i = 0;
+        for i in 0..n {
+            for j in 0..i {
+                if nums[i] % nums[j] == 0 && f[j] > f[i] {
+                    f[i] = f[j];
+                    from[i] = j as i32;
+                }
+            }
+            f[i] += 1;
+            if f[i] > f[max_i] {
+                max_i = i;
+            }
+        }
+
+        let mut path =Vec::new();
+        let mut i = max_i as i32;
+        while i >= 0  {
+            path.push(nums[i as usize]);
+            i = from[i as usize];
+        }
+
+        path
+    }
+}
