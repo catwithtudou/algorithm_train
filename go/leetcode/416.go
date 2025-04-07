58e03d5f87ff89ec1c9e8d7e328e5d5e5cf05400
catwithtudou <949812478@qq.com>
2025-04-07 10:23:28 +0800

    update: done the 416

---
+++ go/leetcode/416.go
@@ -0,0 +1,39 @@
+﻿package leetcode
+
+func canPartition(nums []int) bool {
+	s := 0
+	for _, x := range nums {
+		s += x
+	}
+	if s%2 != 0 {
+		return false
+	}
+
+	n := len(nums)
+	memo := make([][]int8, n)
+	for i := range memo {
+		memo[i] = make([]int8, s/2+1)
+		for j := range memo[i] {
+			memo[i][j] = -1
+		}
+	}
+
+	var dfs func(int, int) bool
+	dfs = func(i, j int) bool {
+		if i < 0 {
+			return j == 0
+		}
+		p := &memo[i][j]
+		if *p != -1 {
+			return *p == 1
+		}
+		res := j >= nums[i] && dfs(i-1, j-nums[i]) || dfs(i-1, j)
+		if res {
+			*p = 1
+		} else {
+			*p = 0
+		}
+		return res
+	}
+	return dfs(n-1, s/2)
+}
---
+++ rust/src/leetcode/four_hundred_and_sixteen.rs
@@ -0,0 +1,31 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn can_partition(nums: Vec<i32>) -> bool {
+        let s = nums.iter().sum::<i32>();
+        if s % 2 != 0 {
+            return false;
+        }
+        let n = nums.len();
+        let mut memo = vec![vec![-1; s as usize / 2 + 1]; n];
+        fn dfs(nums: &Vec<i32>, i: usize, j: usize, memo: &mut Vec<Vec<i32>>) -> bool {
+            if i == 0 {
+                return j == 0;
+            }
+
+            if memo[i][j] != -1 {
+                return memo[i][j] == 1;
+            }
+
+            let res = j as i32 >= nums[i] && dfs(nums, i - 1, j - nums[i] as usize, memo)
+                || dfs(nums, i - 1, j, memo);
+            if res {
+                memo[i][j] = 1;
+            } else {
+                memo[i][j] = 0;
+            }
+            res
+        }
+        dfs(&nums, n - 1, s as usize / 2, &mut memo)
+    }
+}
