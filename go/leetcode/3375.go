70b7d2e755ba6ee2ed1754698b6f7ffad1ab2750
catwithtudou <949812478@qq.com>
2025-04-09 09:57:30 +0800

    update: done the 3375

---
+++ go/leetcode/3375.go
@@ -0,0 +1,24 @@
+﻿package leetcode
+
+import "sort"
+
+func minOperations3375(nums []int, k int) (ans int) {
+	n := len(nums)
+	sort.Ints(nums)
+	if nums[0] < k || nums[n-1] < k {
+		return -1
+	}
+
+	for i := n - 1; i > 0; i-- {
+		if nums[i] == k {
+			continue
+		}
+		if nums[i] != nums[i-1] {
+			ans++
+		}
+	}
+	if nums[0] == k {
+		return ans
+	}
+	return ans + 1
+}
---
+++ rust/src/leetcode/three_thousand_three_hundred_seventy_five.rs
@@ -0,0 +1,14 @@
+﻿use std::collections::HashSet;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
+        let min = *nums.iter().min().unwrap();
+        if k > min {
+            return -1;
+        }
+        let set = nums.into_iter().collect::<HashSet<_>>();
+        set.len() as i32 - if k == min { 1 } else { 0 }
+    }
+}
