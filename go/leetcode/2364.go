a39f295550df15c109cc0d0318c4a8f351adf151
catwithtudou <949812478@qq.com>
2025-04-18 10:23:41 +0800

    update: done the 2365

--- go/leetcode/2364.go
+++ go/leetcode/2364.go
@@ -1,1 +1,13 @@
-﻿
+﻿package leetcode
+
+func countBadPairs(nums []int) int64 {
+	n := len(nums)
+	ans := n * (n - 1) / 2
+	cnt := make(map[int]int)
+	for i, x := range nums {
+		ans -= cnt[x-i]
+		cnt[x-i]++
+	}
+
+	return int64(ans)
+}
--- rust/src/leetcode/two_thousand_three_hundred_sixty_four.rs
+++ rust/src/leetcode/two_thousand_three_hundred_sixty_four.rs
@@ -1,1 +1,17 @@
-﻿
+﻿use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn count_bad_pairs(nums: Vec<i32>) -> i64 {
+        let n = nums.len();
+        let mut ans = n * (n - 1) / 2;
+        let mut cnt = HashMap::new();
+        for i in 0..n {
+            let x = nums[i] - i as i32;
+            ans -= cnt.get(&x).unwrap_or(&0);
+            *cnt.entry(x).or_insert(0) += 1;
+        }
+        ans as i64
+    }
+}
