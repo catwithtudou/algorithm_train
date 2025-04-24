684397e7f81d8c243d30daa71148f46023f22c4b
catwithtudou <949812478@qq.com>
2025-04-24 10:15:54 +0800

    update: done hte 2799

---
+++ go/leetcode/2799.go
@@ -0,0 +1,28 @@
+﻿package leetcode
+
+func countCompleteSubarrays(nums []int) (ans int) {
+
+	set := map[int]struct{}{}
+	for _, v := range nums {
+		set[v] = struct{}{}
+	}
+	k := len(set)
+
+	cnt := map[int]int{}
+	left := 0
+	for _, v := range nums {
+		ans += left
+		cnt[v]++
+		for len(cnt) == k {
+			ans++
+			out := nums[left]
+			cnt[out]--
+			if cnt[out] == 0 {
+				delete(cnt, out)
+			}
+			left++
+		}
+	}
+
+	return
+}
---
+++ rust/src/leetcode/two_thousand_seven_hundred_ninety_nine.rs
@@ -0,0 +1,26 @@
+﻿use std::collections::{HashMap, HashSet};
+
+pub struct Solution;
+
+impl Solution {
+    pub fn count_complete_subarrays(nums: Vec<i32>) -> i32 {
+        let k = nums.iter().collect::<HashSet<_>>().len();
+        let mut ans = 0;
+        let mut left = 0;
+        let mut cnt = HashMap::new();
+        for &x in &nums {
+            *cnt.entry(x).or_insert(0) += 1;
+            while cnt.len() == k {
+                let out = nums[left];
+                let e = cnt.get_mut(&out).unwrap();
+                *e -= 1;
+                if *e == 0 {
+                    cnt.remove(&out);
+                }
+                left += 1;
+            }
+            ans += left;
+        }
+        ans as _
+    }
+}
