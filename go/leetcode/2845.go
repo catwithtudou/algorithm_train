c4dd459503eb81f20bb6b67cb6cbe7fc6b06d969
catwithtudou <949812478@qq.com>
2025-04-25 08:56:07 +0800

    update: done the 2845

---
+++ go/leetcode/2845.go
@@ -0,0 +1,21 @@
+﻿package leetcode
+
+func countInterestingSubarrays(nums []int, modulo int, k int) (ans int64) {
+	sum := make([]int, len(nums)+1)
+	for i, v := range nums {
+		sum[i+1] = sum[i]
+		if v%modulo == k {
+			sum[i+1]++
+		}
+	}
+
+	cnt := make([]int, min(modulo, len(nums)+1))
+	for _, s := range sum {
+		if s >= k {
+			ans += int64(cnt[(s-k)%modulo])
+		}
+		cnt[s%modulo]++
+	}
+
+	return
+}
---
+++ rust/src/leetcode/two_thousand_eight_hundred_forty_five.rs
@@ -0,0 +1,26 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn count_interesting_subarrays(nums: Vec<i32>, modulo: i32, k: i32) -> i64 {
+        let n = nums.len();
+        let mut sum = vec![0; n + 1];
+        for (i, x) in nums.into_iter().enumerate() {
+            sum[i + 1] = sum[i];
+            if x % modulo == k {
+                sum[i + 1] += 1;
+            }
+        }
+
+        let mut ans = 0;
+        let mut cnt = vec![0; (n + 1).min(modulo as usize)];
+
+        for s in sum {
+            if s >= k {
+                ans += cnt[((s - k) % modulo) as usize] as i64;
+            }
+            cnt[(s % modulo) as usize] += 1;
+        }
+
+        ans
+    }
+}
