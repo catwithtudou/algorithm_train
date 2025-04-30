4d6a30439c33c84297550f29cd88d798308e5583
catwithtudou <949812478@qq.com>
2025-04-30 08:49:47 +0800

    update: done the 1295

---
+++ go/leetcode/1295.go
@@ -0,0 +1,22 @@
+﻿package leetcode
+
+func findNumbers(nums []int) (ans int) {
+
+	for _, v := range nums {
+		if v < 10 {
+			continue
+		}
+		if v/10 > 0 && v/10 < 10 {
+			ans++
+			continue
+		} else if v/1000 > 0 && v/1000 < 10 {
+			ans++
+			continue
+		} else if v/100000 > 0 && v/100000 < 10 {
+			ans++
+			continue
+		}
+	}
+
+	return
+}
---
+++ rust/src/leetcode/one_thousand_two_hundred_ninety_five.rs
@@ -0,0 +1,19 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn find_numbers(nums: Vec<i32>) -> i32 {
+        let mut ans = 0;
+
+        for mut x in nums {
+            while x >= 100 {
+                x /= 100;
+            }
+
+            if x >= 10 {
+                ans += 1;
+            }
+        }
+
+        ans
+    }
+}
