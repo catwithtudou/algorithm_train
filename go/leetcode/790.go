e63e7861b06e4caa31fe88433077cf01132dcc9d
catwithtudou <949812478@qq.com>
2025-05-05 16:16:50 +0800

    update: done the 790

---
+++ go/leetcode/790.go
@@ -0,0 +1,15 @@
+﻿package leetcode
+
+func numTilings(n int) int {
+	if n == 1 {
+		return 1
+	}
+	f := make([]int, n+1)
+	f[0], f[1], f[2] = 1, 1, 2
+
+	for i := 3; i <= n; i++ {
+		f[i] = (f[i-1]*2 + f[i-3]) % 1000000007
+	}
+
+	return f[n]
+}
---
+++ rust/src/leetcode/seven_hundred_and_ninety.rs
@@ -0,0 +1,20 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn num_tilings(n: i32) -> i32 {
+        if n == 1 {
+            return 1;
+        }
+        let n = n as usize;
+        let mut f = vec![0; n + 1];
+        f[0] = 1;
+        f[1] = 1;
+        f[2] = 2;
+        for i in 3..=n {
+            let a = (f[i - 1] as i64 * 2) % 1000000007;
+            let b = f[i - 3] as i64;
+            f[i] = ((a + b) % 1000000007) as i32;
+        }
+        f[n]
+    }
+}
