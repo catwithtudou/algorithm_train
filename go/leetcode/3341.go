377f5e368c3014659e38453f13698ec6541b95e8
catwithtudou <949812478@qq.com>
2025-05-07 09:19:43 +0800

    update: done the 3341

---
+++ go/leetcode/3341.go
@@ -0,0 +1,55 @@
+﻿package leetcode
+
+import (
+	"container/heap"
+	"math"
+)
+
+func minTimeToReach(moveTime [][]int) int {
+	n, m := len(moveTime), len(moveTime[0])
+	dis := make([][]int, n)
+	for i := range dis {
+		dis[i] = make([]int, m)
+		for j := range dis[i] {
+			dis[i][j] = math.MaxInt
+		}
+	}
+	dis[0][0] = 0
+
+	dirs := []struct{ x, y int }{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+
+	h := hpDirs{{}}
+
+	for {
+		top := heap.Pop(&h).(tupleDirs)
+		i, j := top.x, top.y
+		if i == n-1 && j == m-1 {
+			return top.dis
+		}
+
+		if top.dis > dis[i][j] {
+			continue
+		}
+
+		for _, d := range dirs {
+			x, y := i+d.x, j+d.y
+			if x >= 0 && x < n && y >= 0 && y < m {
+				newDis := max(top.dis, moveTime[x][y]) + 1
+				if newDis < dis[x][y] {
+					dis[x][y] = newDis
+					heap.Push(&h, tupleDirs{x, y, newDis})
+				}
+			}
+		}
+	}
+
+}
+
+type tupleDirs struct{ x, y, dis int }
+type hpDirs []tupleDirs
+
+func (h hpDirs) Len() int           { return len(h) }
+func (h hpDirs) Less(i, j int) bool { return h[i].dis < h[j].dis }
+func (h hpDirs) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *hpDirs) Push(v any)        { *h = append(*h, v.(tupleDirs)) }
+func (h *hpDirs) Pop() (v any)      { a := *h; *h, v = a[:len(a)-1], a[len(a)-1]; return }
---
+++ rust/src/leetcode/three_thousand_three_hundred_forty_one.rs
@@ -0,0 +1,47 @@
+﻿pub struct Solution;
+
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+impl Solution {
+    pub fn min_time_to_reach(move_time: Vec<Vec<i32>>) -> i32 {
+        let n = move_time.len();
+        let m = move_time[0].len();
+
+        let mut dis = vec![vec![i32::MAX; m]; n];
+        dis[0][0] = 0;
+
+        let dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+        let mut heap = BinaryHeap::new();
+        heap.push(Reverse((0, 0, 0))); // 小顶堆
+
+        while let Some(Reverse((distance, i, j))) = heap.pop() {
+            if i == n - 1 && j == m - 1 {
+                return distance;
+            }
+
+            if distance > dis[i][j] {
+                continue;
+            }
+
+            for &(dx, dy) in &dirs {
+                let x = i as i32 + dx;
+                let y = j as i32 + dy;
+
+                if x >= 0 && x < n as i32 && y >= 0 && y < m as i32 {
+                    let x = x as usize;
+                    let y = y as usize;
+                    let new_dis = std::cmp::max(distance, move_time[x][y]) + 1;
+
+                    if new_dis < dis[x][y] {
+                        dis[x][y] = new_dis;
+                        heap.push(Reverse((new_dis, x, y)));
+                    }
+                }
+            }
+        }
+
+        -1
+    }
+}
