cc1dc832b29dc48e24f5a6015c73844d5a3f23e9
catwithtudou <949812478@qq.com>
2025-05-13 08:56:32 +0800

    update: done the 3335

---
+++ go/leetcode/3335.go
@@ -0,0 +1,24 @@
+﻿package leetcode
+
+func lengthAfterTransformations(s string, t int) (ans int) {
+	cnt := [26]int{}
+	for _, c := range s {
+		cnt[c-'a']++
+	}
+
+	for i := 0; i < t; i++ {
+		nxt := [26]int{}
+		nxt[0] = cnt[25]
+		nxt[1] = (cnt[0] + cnt[25]) % mod
+		for j := 2; j < 26; j++ {
+			nxt[j] = cnt[j-1]
+		}
+		cnt = nxt
+	}
+
+	for i := 0; i < 26; i++ {
+		ans = (ans + cnt[i]) % mod
+	}
+
+	return
+}
---
+++ rust/src/leetcode/three_thousand_three_hundred_thirty_five.rs
@@ -0,0 +1,28 @@
+﻿pub struct Solution;
+
+impl Solution {
+    const MOD: i32 = 1_000_000_007;
+
+    pub fn length_after_transformations(s: String, t: i32) -> i32 {
+        let mut cnt = [0; 26];
+        for ch in s.chars() {
+            cnt[(ch as u8 - b'a') as usize] += 1;
+        }
+
+        for _ in 0..t {
+            let mut nxt = [0; 26];
+            nxt[0] = cnt[25];
+            nxt[1] = (cnt[0] + cnt[25]) % Self::MOD;
+            for j in 2..26 {
+                nxt[j] = cnt[j - 1];
+            }
+            cnt = nxt;
+        }
+
+        let mut ans = 0;
+        for &num in cnt.iter() {
+            ans = (ans + num) % Self::MOD;
+        }
+        ans
+    }
+}
