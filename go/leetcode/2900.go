0e080ade2c97d70fa237f81a63282bc74978e1d5
catwithtudou <949812478@qq.com>
2025-05-15 08:56:36 +0800

    update: done the 2900

---
+++ go/leetcode/2900.go
@@ -0,0 +1,15 @@
+﻿package leetcode
+
+func getLongestSubsequence(words []string, groups []int) (ans []string) {
+
+	pre := groups[0]
+	ans = append(ans, words[0])
+	for i := 1; i < len(words); i++ {
+		if groups[i] != pre {
+			ans = append(ans, words[i])
+			pre = groups[i]
+		}
+	}
+
+	return
+}
---
+++ rust/src/leetcode/two_thousand_and_nine_hundred.rs
@@ -0,0 +1,14 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn get_longest_subsequence(words: Vec<String>, groups: Vec<i32>) -> Vec<String> {
+        let n = groups.len();
+        let mut ans = vec![];
+        for (i, word) in words.into_iter().enumerate() {
+            if i == n - 1 || groups[i] != groups[i + 1] {
+                ans.push(word);
+            }
+        }
+        ans
+    }
+}
