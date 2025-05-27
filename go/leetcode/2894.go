0d5bce5b1dfc7410e0a408b7225f53040e590920
catwithtudou <949812478@qq.com>
2025-05-27 08:46:51 +0800

    update: done the 2894

---
+++ go/leetcode/2894.go
@@ -0,0 +1,12 @@
+﻿package leetcode
+
+func differenceOfSums(n int, m int) int {
+	sum, num1 := 0, 0
+	for i := 1; i <= n; i++ {
+		sum += i
+		if i%m == 0 {
+			num1 += i
+		}
+	}
+	return sum - num1*2
+}
---
+++ rust/src/leetcode/two_thousand_eight_hundred_ninety_four.rs
@@ -0,0 +1,7 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn difference_of_sums(n: i32, m: i32) -> i32 {
+        n * (n + 1) / 2 - n / m * (n / m + 1) * m
+    }
+}
