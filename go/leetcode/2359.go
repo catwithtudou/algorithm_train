309d8c9bef0258a0a6969a3bc30e684875930be4
catwithtudou <949812478@qq.com>
2025-05-30 23:28:40 +0800

    update: done the 2359

---
+++ go/leetcode/2359.go
@@ -0,0 +1,28 @@
+﻿package leetcode
+
+func closestMeetingNode(edges []int, node1 int, node2 int) int {
+	n := len(edges)
+	calcDis := func(x int) []int {
+		dis := make([]int, n)
+		for i := range dis {
+			dis[i] = n
+		}
+		for d := 0; x >= 0 && dis[x] == n; x = edges[x] {
+			dis[x] = d
+			d++
+		}
+		return dis
+	}
+
+	dis1 := calcDis(node1)
+	dis2 := calcDis(node2)
+
+	minDis, ans := n, -1
+	for i, d1 := range dis1 {
+		d := max(d1, dis2[i])
+		if d < minDis {
+			minDis, ans = d, i
+		}
+	}
+	return ans
+}
---
+++ rust/src/leetcode/two_thousand_three_hundred_fifty_nine.rs
@@ -0,0 +1,33 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn closest_meeting_node(edges: Vec<i32>, node1: i32, node2: i32) -> i32 {
+        let n = edges.len();
+
+        let calc_dis = |start: i32| -> Vec<usize> {
+            let (mut dis, mut x, mut d) = (vec![n; n], start, 0);
+
+            while x >= 0 && dis[x as usize] == n {
+                dis[x as usize] = d;
+                d += 1;
+                x = edges[x as usize];
+            }
+
+            dis
+        };
+
+        let (dis1, dis2) = (calc_dis(node1), calc_dis(node2));
+
+        let (mut min_dis, mut ans) = (n, -1);
+
+        for (i, &d1) in dis1.iter().enumerate() {
+            let d = d1.max(dis2[i]);
+            if d < min_dis {
+                min_dis = d;
+                ans = i as i32;
+            }
+        }
+
+        ans
+    }
+}
