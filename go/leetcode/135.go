90f99bf40e5a6ce66f4b93f69edb3eefd8678133
catwithtudou <949812478@qq.com>
2025-06-02 10:58:30 +0800

    update: done the 135

---
+++ go/leetcode/135.go
@@ -0,0 +1,23 @@
+﻿package leetcode
+
+func candy(ratings []int) (ans int) {
+	n := len(ratings)
+	left, right := make([]int, n), make([]int, n)
+	for i := range n {
+		left[i], right[i] = 1, 1
+	}
+	for i := 1; i < n; i++ {
+		if ratings[i] > ratings[i-1] {
+			left[i] = left[i-1] + 1
+		}
+	}
+	ans = left[n-1]
+	for i := n - 2; i >= 0; i-- {
+		if ratings[i] > ratings[i+1] {
+			right[i] = right[i+1] + 1
+		}
+		ans += max(left[i], right[i])
+	}
+
+	return
+}
---
+++ rust/src/leetcode/one_hundred_and_thirty_five.rs
@@ -0,0 +1,30 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn candy(ratings: Vec<i32>) -> i32 {
+        let n = ratings.len();
+        let mut ans = n;
+        let mut i = 0;
+        while i < n {
+            let start = if i > 0 && ratings[i] > ratings[i - 1] {
+                i - 1
+            } else {
+                i
+            };
+
+            while i + 1 < n && ratings[i + 1] > ratings[i] {
+                i += 1;
+            }
+            let top = i;
+            while i + 1 < n && ratings[i + 1] < ratings[i] {
+                i += 1;
+            }
+            let inc = top - start;
+            let dec = i - top;
+            ans += (inc * (inc - 1) + dec * (dec - 1)) / 2 + inc.max(dec);
+            i += 1;
+        }
+
+        ans as _
+    }
+}
