d53ad3d3e811a3e1161979b33027301c60b67d42
catwithtudou <949812478@qq.com>
2025-06-03 19:29:48 +0800

    update: done the 1298

---
+++ go/leetcode/1298.go
@@ -0,0 +1,37 @@
+﻿package leetcode
+
+func maxCandies(status []int, candies []int, keys [][]int, containedBoxes [][]int, initialBoxes []int) (ans int) {
+	hasKey := status
+	hasBox := make([]bool, len(status))
+	for _, v := range initialBoxes {
+		hasBox[v] = true
+	}
+
+	var dfs func(int)
+	dfs = func(x int) {
+		ans += candies[x]
+		hasBox[x] = false
+
+		for _, y := range keys[x] {
+			hasKey[y] = 1
+			if hasBox[y] {
+				dfs(y)
+			}
+		}
+
+		for _, y := range containedBoxes[x] {
+			hasBox[y] = true
+			if hasKey[y] == 1 {
+				dfs(y)
+			}
+		}
+	}
+
+	for _, x := range initialBoxes {
+		if hasKey[x] == 1 && hasBox[x] {
+			dfs(x)
+		}
+	}
+
+	return
+}
---
+++ rust/src/leetcode/one_thousand_two_hundred_ninety_eight.rs
@@ -0,0 +1,78 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn max_candies(
+        status: Vec<i32>,
+        candies: Vec<i32>,
+        keys: Vec<Vec<i32>>,
+        contained_boxes: Vec<Vec<i32>>,
+        initial_boxes: Vec<i32>,
+    ) -> i32 {
+        let mut has_key = status;
+        let mut has_box = vec![false; has_key.len()];
+        for &x in &initial_boxes {
+            has_box[x as usize] = true;
+        }
+
+        fn dfs(
+            x: usize,
+            candies: &Vec<i32>,
+            keys: &[Vec<i32>],
+            contained_boxes: &[Vec<i32>],
+            has_key: &mut Vec<i32>,
+            has_box: &mut Vec<bool>,
+            ans: &mut i32,
+        ) {
+            *ans += candies[x];
+            has_box[x] = false;
+
+            for &y in &keys[x] {
+                has_key[y as usize] = 1;
+                if has_box[y as usize] {
+                    dfs(
+                        y as usize,
+                        candies,
+                        keys,
+                        contained_boxes,
+                        has_key,
+                        has_box,
+                        ans,
+                    );
+                }
+            }
+
+            for &y in &contained_boxes[x] {
+                has_box[y as usize] = true;
+                if has_key[y as usize] == 1 {
+                    dfs(
+                        y as usize,
+                        candies,
+                        keys,
+                        contained_boxes,
+                        has_key,
+                        has_box,
+                        ans,
+                    );
+                }
+            }
+        }
+
+        let mut ans = 0;
+        for x in initial_boxes {
+            let x = x as usize;
+            if has_box[x] && has_key[x] == 1 {
+                dfs(
+                    x,
+                    &candies,
+                    &keys,
+                    &contained_boxes,
+                    &mut has_key,
+                    &mut has_box,
+                    &mut ans,
+                );
+            }
+        }
+
+        ans
+    }
+}
