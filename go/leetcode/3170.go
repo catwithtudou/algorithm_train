da7a75823e268c9f3caa27c83cebaef0bbf71ecf
catwithtudou <949812478@qq.com>
2025-06-07 15:40:02 +0800

    update: done the 3170

---
+++ go/leetcode/3170.go
@@ -0,0 +1,33 @@
+﻿package leetcode
+
+import (
+	"golang.org/x/exp/slices"
+)
+
+func clearStars(s string) string {
+	stack := make([][]int, 26)
+	for i, c := range s {
+		if c != '*' {
+			stack[c-'a'] = append(stack[c-'a'], i)
+			continue
+		}
+		for j, st := range stack {
+			if len(st) > 0 {
+				stack[j] = st[:len(st)-1]
+				break
+			}
+		}
+	}
+
+	idx := []int{}
+	for _, p := range stack {
+		idx = append(idx, p...)
+	}
+	slices.Sort(idx)
+
+	ans := make([]byte, len(idx))
+	for i, p := range idx {
+		ans[i] = s[p]
+	}
+	return string(ans)
+}
---
+++ rust/src/leetcode/three_thousand_one_hundred_and_seventy.rs
@@ -0,0 +1,30 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn clear_stars(s: String) -> String {
+        let mut stack = vec![vec![]; 26];
+        for (i, &c) in s.as_bytes().iter().enumerate() {
+            if c != b'*' {
+                stack[(c - b'a') as usize].push(i);
+                continue;
+            }
+
+            for st in stack.iter_mut() {
+                if !st.is_empty() {
+                    st.pop();
+                    break;
+                }
+            }
+        }
+
+        let mut idx: Vec<usize> = Vec::new();
+        for st in &stack {
+            idx.extend(st);
+        }
+        idx.sort();
+
+        let s_bytes = s.as_bytes();
+        let ans: Vec<u8> = idx.iter().map(|&i| s_bytes[i]).collect();
+        String::from_utf8(ans).unwrap()
+    }
+}
