611d7e8d0fc1bdf11f087629ccc44968aa1ea038
catwithtudou <949812478@qq.com>
2025-06-10 08:42:00 +0800

    update: done the 3442

---
+++ go/leetcode/3442.go
@@ -0,0 +1,19 @@
+﻿package leetcode
+
+import "math"
+
+func maxDifference(s string) int {
+	cnt := make([]int, 26)
+	for _, c := range s {
+		cnt[c-'a']++
+	}
+	maxJ, minO := 0, math.MaxInt
+	for _, c := range cnt {
+		if c%2 > 0 {
+			maxJ = max(maxJ, c)
+		} else if c > 0 {
+			minO = min(minO, c)
+		}
+	}
+	return maxJ - minO
+}
---
+++ rust/src/leetcode/three_thousand_four_hundred_forty_two.rs
@@ -0,0 +1,19 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn max_difference(s: String) -> i32 {
+        let mut cnt = [0; 26];
+        for c in s.bytes() {
+            cnt[(c - b'a') as usize] += 1;
+        }
+        let (mut maxJ, mut minO) = (0, 100);
+        for c in cnt {
+            if c % 2 > 0 {
+                maxJ = maxJ.max(c);
+            } else if c > 0 {
+                minO = minO.min(c);
+            }
+        }
+        maxJ - minO
+    }
+}
