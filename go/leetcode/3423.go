7c4524df0baa3eda4cce87d56639b5dec978332e
catwithtudou <949812478@qq.com>
2025-06-12 09:53:57 +0800

    update: done the 3423

---
+++ go/leetcode/3423.go
@@ -0,0 +1,9 @@
+﻿package leetcode
+
+func maxAdjacentDistance(nums []int) (ans int) {
+	n := len(nums)
+	for i := 1; i <= n; i++ {
+		ans = max(ans, abs(nums[i%n]-nums[(i-1)%n]))
+	}
+	return
+}
---
+++ rust/src/leetcode/three_thousand_four_hundred_twenty_three.rs
@@ -0,0 +1,10 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn max_adjacent_distance(nums: Vec<i32>) -> i32 {
+        (0..nums.len())
+            .map(|i| (nums[(i + 1) % nums.len()] - nums[i]).abs())
+            .max()
+            .unwrap_or(0)
+    }
+}
