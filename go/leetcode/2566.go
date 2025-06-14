3a5a5454a1e17f8f3c036b736677f8c86c75cd16
catwithtudou <949812478@qq.com>
2025-06-14 10:36:52 +0800

    update: done the 2566

---
+++ go/leetcode/2566.go
@@ -0,0 +1,20 @@
+﻿package leetcode
+
+import (
+	"strconv"
+	"strings"
+)
+
+func minMaxDifference(num int) int {
+	numStr := strconv.Itoa(num)
+	mx := num
+	for _, c := range numStr {
+		if c != '9' {
+			mx, _ = strconv.Atoi(strings.ReplaceAll(numStr, string(c), "9"))
+			break
+		}
+	}
+	mn, _ := strconv.Atoi(strings.ReplaceAll(numStr, numStr[:1], "0"))
+
+	return mx - mn
+}
---
+++ rust/src/leetcode/two_thousand_five_hundred_sixty_six.rs
@@ -0,0 +1,20 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn min_max_difference(num: i32) -> i32 {
+        let num_str = num.to_string();
+        let mut mx = num;
+
+        for c in num_str.chars() {
+            if c != '9' {
+                mx = num_str.replace(c, "9").parse::<i32>().unwrap();
+                break;
+            }
+        }
+
+        let first_char = num_str.chars().next().unwrap();
+        let mn = num_str.replace(first_char, "0").parse::<i32>().unwrap();
+
+        mx - mn
+    }
+}
