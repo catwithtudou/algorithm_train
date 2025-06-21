66bbfe9eb924686ccd2740d1f2356cb549efa651
catwithtudou <949812478@qq.com>
2025-06-21 10:43:29 +0800

    update: done the 3085

---
+++ go/leetcode/3085.go
@@ -0,0 +1,21 @@
+﻿package leetcode
+
+import "slices"
+
+func minimumDeletions(word string, k int) int {
+	cnt := make([]int, 26)
+	for _, c := range word {
+		cnt[c-'a']++
+	}
+	slices.Sort(cnt)
+
+	maxSave := 0
+	for i, base := range cnt {
+		sum := 0
+		for _, c := range cnt[i:] {
+			sum += min(c, base+k)
+		}
+		maxSave = max(maxSave, sum)
+	}
+	return len(word) - maxSave
+}
---
+++ rust/src/leetcode/three_thousand_and_eighty_five.rs
@@ -0,0 +1,22 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn minimum_deletions(word: String, k: i32) -> i32 {
+        let mut cnt = vec![0; 26];
+        for c in word.bytes() {
+            cnt[(c - b'a') as usize] += 1;
+        }
+        cnt.sort_unstable();
+
+        let mut max_save = 0;
+
+        for (i, &base) in cnt.iter().enumerate() {
+            let mut sum = 0;
+            for &c in &cnt[i..] {
+                sum += c.min(base + k);
+            }
+            max_save = max_save.max(sum);
+        }
+        word.len() as i32- max_save
+    }
+}
