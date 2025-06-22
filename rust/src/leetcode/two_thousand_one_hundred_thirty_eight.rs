f464fef08f61df1d9ba383b23e7aa798c1e1f31f
catwithtudou <949812478@qq.com>
2025-06-22 21:52:09 +0800

    update: done the 2138

---
+++ rust/src/leetcode/two_thousand_one_hundred_thirty_eight.rs
@@ -0,0 +1,17 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn divide_string(s: String, k: i32, fill: char) -> Vec<String> {
+        let k = k as usize;
+        s.into_bytes()
+            .chunks(k)
+            .map(|chunk| {
+                let mut t = chunk.to_vec();
+                if t.len() < k {
+                    t.extend(std::iter::repeat(fill as u8).take(k - t.len()));
+                }
+                unsafe { String::from_utf8_unchecked(t) }
+            })
+            .collect()
+    }
+}
