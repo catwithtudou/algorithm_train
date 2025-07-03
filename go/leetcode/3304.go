338fb4a09f05fb73943d6877d88518d55d25b0da
catwithtudou <949812478@qq.com>
2025-07-03 09:42:53 +0800

    update: done the 3304

---
+++ go/leetcode/3304.go
@@ -0,0 +1,7 @@
+﻿package leetcode
+
+import "math/bits"
+
+func kthCharacter(k int) byte {
+	return 'a' + byte(bits.OnesCount(uint(k-1)))
+}
---
+++ rust/src/leetcode/three_thousand_three_hundred_and_four.rs
@@ -0,0 +1,8 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn kth_character(k: i32) -> char {
+        let ones_count = (k - 1).count_ones() as u8;
+        (b'a' + ones_count) as char
+    }
+}
