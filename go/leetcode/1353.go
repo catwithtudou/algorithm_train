5d43f4dccaf503443d1edd56a78df30a5d038807
catwithtudou <949812478@qq.com>
2025-07-07 10:19:48 +0800

    update: done the 1353

---
+++ go/leetcode/1353.go
@@ -0,0 +1,45 @@
+﻿package leetcode
+
+import (
+	"container/heap"
+	"sort"
+)
+
+func maxEvents(events [][]int) (ans int) {
+	mx := 0
+	for _, v := range events {
+		mx = max(mx, v[1])
+	}
+
+	group := make([][]int, mx+1)
+	for _, v := range events {
+		group[v[0]] = append(group[v[0]], v[1])
+	}
+
+	h := &maxEventsHp{}
+
+	for i, g := range group {
+		for h.Len() > 0 && h.IntSlice[0] < i {
+			heap.Pop(h)
+		}
+
+		for _, end := range g {
+			heap.Push(h, end)
+		}
+
+		if h.Len() > 0 {
+			ans++
+			heap.Pop(h)
+		}
+	}
+	return
+}
+
+type maxEventsHp struct{ sort.IntSlice }
+
+func (h *maxEventsHp) Push(v any) { h.IntSlice = append(h.IntSlice, v.(int)) }
+func (h *maxEventsHp) Pop() any {
+	v := h.IntSlice[len(h.IntSlice)-1]
+	h.IntSlice = h.IntSlice[:len(h.IntSlice)-1]
+	return v
+}
---
+++ rust/src/leetcode/one_thousand_three_hundred_fifty_three.rs
@@ -0,0 +1,39 @@
+﻿use std::collections::BinaryHeap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn max_events(events: Vec<Vec<i32>>) -> i32 {
+        let mut mx = 0;
+        for e in &events {
+            mx = mx.max(e[1]);
+        }
+
+        let mut cnt = vec![vec![]; (mx + 1) as usize];
+        for e in events {
+            cnt[e[0] as usize].push(e[1]);
+        }
+
+        let mut ans = 0;
+        let mut h = BinaryHeap::<i32>::new();
+
+        for (i, g) in cnt.into_iter().enumerate() {
+            while let Some(end) = h.peek() {
+                if -end >= i as i32 {
+                    break;
+                }
+                h.pop();
+            }
+
+            for end in g {
+                h.push(-end);
+            }
+
+            if let Some(_) = h.pop() {
+                ans += 1;
+            }
+        }
+
+        ans
+    }
+}
