149f6b047800098c7503e5eea939212759f9cfd3
catwithtudou <949812478@qq.com>
2025-07-09 22:15:56 +0800

    update: done the 3439

---
+++ go/leetcode/3439.go
@@ -0,0 +1,23 @@
+﻿package leetcode
+
+func maxFreeTime(eventTime int, k int, startTime []int, endTime []int) (ans int) {
+	n := len(startTime)
+	free := make([]int, n+1)
+	free[0] = startTime[0]
+	for i := 1; i < n; i++ {
+		free[i] = startTime[i] - endTime[i-1]
+	}
+	free[n] = eventTime - endTime[n-1]
+
+	s := 0
+	for i, f := range free {
+		s += f
+		if i < k {
+			continue
+		}
+		ans = max(ans, s)
+		s -= free[i-k]
+	}
+
+	return
+}
---
+++ rust/src/leetcode/three_thousand_four_hundred_thirty_nine.rs
@@ -0,0 +1,26 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn max_free_time(event_time: i32, k: i32, start_time: Vec<i32>, end_time: Vec<i32>) -> i32 {
+        let n = start_time.len();
+        let k = k as usize;
+        let mut free = vec![0; n + 1];
+        free[0] = start_time[0];
+        for i in 1..n {
+            free[i] = start_time[i] - end_time[i - 1];
+        }
+        free[n] = event_time - end_time[n - 1];
+
+        let mut s = 0;
+        let mut ans = 0;
+        for i in 0..n + 1 {
+            s += free[i];
+            if i < k {
+                continue;
+            }
+            ans = ans.max(s);
+            s -= free[i - k];
+        }
+        ans
+    }
+}
