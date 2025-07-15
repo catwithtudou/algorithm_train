6d8daea59959b6ba5ac0a11b9002f8a50d3742f9
catwithtudou <949812478@qq.com>
2025-07-15 21:44:47 +0800

    update: done the 3136

---
+++ rust/src/leetcode/three_thousand_one_hundred_thirty_six.rs
@@ -0,0 +1,26 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn is_valid(word: String) -> bool {
+        if word.len() < 3 {
+            return false;
+        }
+
+        let mut f0 = false;
+        let mut f1 = false;
+
+        for c in word.chars() {
+            if c.is_alphabetic() {
+                if "aeiou".contains(c.to_lowercase().next().unwrap()) {
+                    f1 = true;
+                } else {
+                    f0 = true;
+                }
+            } else if !c.is_numeric() {
+                return false;
+            }
+        }
+
+        f0 && f1
+    }
+}
