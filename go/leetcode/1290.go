23f1db3a876e63f2128af78f294f09f6ecd908db
catwithtudou <949812478@qq.com>
2025-07-15 21:50:17 +0800

    update: update the 1290

---
+++ go/leetcode/1290.go
@@ -0,0 +1,16 @@
+﻿package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getDecimalValue(head *ListNode) (ans int) {
+	for head != nil {
+		ans = ans*2 + head.Val
+		head = head.Next
+	}
+	return
+}
--- rust/src/leetcode.rs
+++ rust/src/leetcode.rs
@@ -130,6 +130,7 @@ pub mod one_thousand_two_hundred_sixty_one;
 pub mod one_thousand_two_hundred_seventy_six;
 pub mod one_thousand_two_hundred_seventy_eight;
 pub mod one_thousand_two_hundred_eighty_seven;
+pub mod one_thousand_two_hundred_and_ninety;
 pub mod one_thousand_two_hundred_ninety_five;
 pub mod one_thousand_two_hundred_ninety_eight;
 pub mod one_thousand_two_hundred_ninety_nine;
