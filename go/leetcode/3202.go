5e04370d3d56ad84d654ca5e6edf541ddbe33662
catwithtudou <949812478@qq.com>
2025-07-17 09:37:15 +0800

    update: done the 3202

---
+++ go/leetcode/3202.go
@@ -0,0 +1,17 @@
+﻿package leetcode
+
+func maximumLength3202(nums []int, k int) (ans int) {
+	f := make([][]int, k)
+	for i := range f {
+		f[i] = make([]int, k)
+	}
+
+	for _, x := range nums {
+		x %= k
+		for y, fxy := range f[x] {
+			f[y][x] = fxy + 1
+			ans = max(ans, f[y][x])
+		}
+	}
+	return
+}
---
+++ rust/src/leetcode/three_thousand_two_hundred_and_two.rs
@@ -0,0 +1,19 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn maximum_length(nums: Vec<i32>, k: i32) -> i32 {
+        let mut ans = 0;
+        let k = k as usize;
+        let mut f = vec![vec![0; k]; k];
+
+        for x in nums {
+            let x = (x % k as i32) as usize;
+            for y in 0..k {
+                let fxy = f[x][y];
+                f[y][x] = fxy + 1;
+                ans = ans.max(f[y][x]);
+            }
+        }
+        ans
+    }
+}
