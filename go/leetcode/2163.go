f637ee635fe6cfb43192cdf4081b3f3d8d46d015
catwithtudou <949812478@qq.com>
2025-07-18 10:14:21 +0800

    update: done the 2163

---
+++ go/leetcode/2163.go
@@ -0,0 +1,57 @@
+﻿package leetcode
+
+import (
+	"container/heap"
+	"sort"
+)
+
+func minimumDifference(nums []int) int64 {
+	m := len(nums)
+	n := m / 3
+	minH := minHeap{nums[m-n:]}
+	heap.Init(&minH)
+	sum := 0
+	for _, v := range nums[m-n:] {
+		sum += v
+	}
+
+	sufMax := make([]int, m-n+1)
+	sufMax[m-n] = sum
+	for i := m - n - 1; i >= n; i-- {
+		if v := nums[i]; v > minH.IntSlice[0] {
+			sum += v - minH.IntSlice[0]
+			minH.IntSlice[0] = v
+			heap.Fix(&minH, 0)
+		}
+		sufMax[i] = sum
+	}
+
+	maxH := maxDiffHeap{nums[:n]}
+	heap.Init(&maxH)
+	preMin := 0
+	for _, v := range nums[:n] {
+		preMin += v
+	}
+
+	ans := preMin - sufMax[n]
+	for i := n; i < m-n; i++ {
+		if v := nums[i]; v < maxH.IntSlice[0] {
+			preMin += v - maxH.IntSlice[0]
+			maxH.IntSlice[0] = v
+			heap.Fix(&maxH, 0)
+		}
+		ans = min(ans, preMin-sufMax[i+1])
+	}
+	return int64(ans)
+}
+
+type minHeap struct{ sort.IntSlice }
+
+func (minHeap) Push(any)     {}
+func (minHeap) Pop() (_ any) { return }
+
+type maxDiffHeap struct{ sort.IntSlice }
+
+func (maxDiffHeap) Push(any)             {}
+func (maxDiffHeap) Pop() (_ any)         { return }
+func (h maxDiffHeap) Less(i, j int) bool { return h.IntSlice[i] > h.IntSlice[j] }
---
+++ rust/src/leetcode/two_thousand_one_hundred_sixty_three.rs
@@ -0,0 +1,53 @@
+﻿pub struct Solution;
+
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+impl Solution {
+    pub fn minimum_difference(nums: Vec<i32>) -> i64 {
+        let m = nums.len();
+        let n = m / 3;
+
+        // 创建最小堆来维护后n个最大元素
+        let mut min_heap: BinaryHeap<Reverse<i32>> =
+            nums[m - n..].iter().map(|&x| Reverse(x)).collect();
+
+        let mut sum: i64 = nums[m - n..].iter().map(|&x| x as i64).sum();
+
+        let mut suf_max = vec![0i64; m - n + 1];
+        suf_max[m - n] = sum;
+
+        for i in (n..m - n).rev() {
+            let v = nums[i];
+            if let Some(&Reverse(min_val)) = min_heap.peek() {
+                if v > min_val {
+                    sum += (v - min_val) as i64;
+                    min_heap.pop();
+                    min_heap.push(Reverse(v));
+                }
+            }
+            suf_max[i] = sum;
+        }
+
+        // 创建最大堆来维护前n个最小元素
+        let mut max_heap: BinaryHeap<i32> = nums[..n].iter().cloned().collect();
+
+        let mut pre_min: i64 = nums[..n].iter().map(|&x| x as i64).sum();
+
+        let mut ans = pre_min - suf_max[n];
+
+        for i in n..m - n {
+            let v = nums[i];
+            if let Some(&max_val) = max_heap.peek() {
+                if v < max_val {
+                    pre_min += (v - max_val) as i64;
+                    max_heap.pop();
+                    max_heap.push(v);
+                }
+            }
+            ans = ans.min(pre_min - suf_max[i + 1]);
+        }
+
+        ans
+    }
+}
