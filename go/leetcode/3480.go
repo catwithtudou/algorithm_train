6b939a9f4df4ecd84321af987cd3dcc2287d58d4
catwithtudou <949812478@qq.com>
2025-07-26 08:23:49 +0800

    update: done the 3480

---
+++ go/leetcode/3480.go
@@ -0,0 +1,28 @@
+﻿package leetcode
+
+import "slices"
+
+func maxSubarrays(n int, conflictingPairs [][]int) int64 {
+	group := make([][]int, n+1)
+	for _, p := range conflictingPairs {
+		a, b := p[0], p[1]
+		if a > b {
+			a, b = b, a
+		}
+		group[a] = append(group[a], b)
+	}
+
+	extra := make([]int, n+2)
+	b := []int{n + 1, n + 1}
+	ans := 0
+
+	for i := n; i > 0; i-- {
+		b = append(b, group[i]...)
+		slices.Sort(b)
+		b = b[:2]
+		ans += b[0] - i
+		extra[b[0]] += b[1] - b[0]
+	}
+
+	return int64(ans + slices.Max(extra))
+}
---
+++ rust/src/leetcode/three_thousand_four_hundred_and_eighty.rs
@@ -0,0 +1,34 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn max_subarrays(n: i32, conflicting_pairs: Vec<Vec<i32>>) -> i64 {
+        let n = n as usize;
+        let mut group = vec![vec![]; n + 1];
+
+        for pair in &conflicting_pairs {
+            let mut a = pair[0] as usize;
+            let mut b = pair[1] as usize;
+            if a > b {
+                std::mem::swap(&mut a, &mut b);
+            }
+            group[a].push(b);
+        }
+
+        let mut extra = vec![0; n + 2];
+        let mut b = vec![n + 1, n + 1];
+        let mut ans = 0;
+
+        for i in (1..=n).rev() {
+            b.extend_from_slice(&group[i]);
+            b.sort();
+            b.truncate(2);
+
+            ans += b[0] - i;
+            extra[b[0]] += b[1] - b[0];
+        }
+
+        let max_extra = extra.iter().max().unwrap_or(&0);
+
+        (ans + max_extra) as i64
+    }
+}
