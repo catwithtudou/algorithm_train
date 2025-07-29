32feb1733b12d0280e74e06be79e0011a5335f70
catwithtudou <949812478@qq.com>
2025-07-29 09:22:08 +0800

    update: done the 2411

---
+++ go/leetcode/2411.go
@@ -0,0 +1,15 @@
+﻿package leetcode
+
+func smallestSubarrays(nums []int) []int {
+	ans := make([]int, len(nums))
+
+	for i, x := range nums {
+		ans[i] = 1
+		for j := i - 1; j >= 0 && nums[j]|x != nums[j]; j-- {
+			nums[j] |= x
+			ans[j] = i - j + 1
+		}
+	}
+
+	return ans
+}
---
+++ rust/src/leetcode/two_thousand_four_hundred_and_eleven.rs
@@ -0,0 +1,20 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn smallest_subarrays(mut nums: Vec<i32>) -> Vec<i32> {
+        let mut ans = vec![1; nums.len()];
+
+        for i in 0..nums.len() {
+            let x = nums[i];
+            for j in (0..i).rev() {
+                if (nums[j] | x) == nums[j] {
+                    break;
+                }
+                nums[j] |= x;
+                ans[j] = (i - j) as i32 + 1;
+            }
+        }
+
+        ans
+    }
+}
