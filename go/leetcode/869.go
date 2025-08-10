9a26a3f5cbf93aa9bc339c0606c26c2fdd1f1238
catwithtudou <949812478@qq.com>
2025-08-10 20:27:20 +0800

    update: done the 868

---
+++ go/leetcode/869.go
@@ -0,0 +1,27 @@
+﻿package leetcode
+
+import (
+	"slices"
+	"strconv"
+)
+
+var powTwoSortedStrSet = map[string]bool{}
+
+func init869() {
+	const maxN = 1_000_000_000
+	for i := 1; i <= maxN; i <<= 1 {
+		s := intToSortdStr(i)
+		powTwoSortedStrSet[s] = true
+	}
+}
+
+func intToSortdStr(n int) string {
+	s := []byte(strconv.Itoa(n))
+	slices.Sort(s)
+	return string(s)
+}
+
+func reorderedPowerOf2(n int) bool {
+	s := intToSortdStr(n)
+	return powTwoSortedStrSet[s]
+}
---
+++ rust/src/leetcode/eight_hundread_and_sixty_nine.rs
@@ -0,0 +1,31 @@
+﻿pub struct Solution;
+
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn reordered_power_of2(n: i32) -> bool {
+        fn get_power_of_two_sorted_set() -> HashSet<String> {
+            let mut set = HashSet::new();
+            let mut power = 1i64; // 使用 i64 避免溢出
+
+            while power <= 1_000_000_000 {
+                let sorted_str = int_to_sorted_str(power as i32);
+                set.insert(sorted_str);
+                power <<= 1;
+            }
+
+            set
+        }
+
+        fn int_to_sorted_str(n: i32) -> String {
+            let mut chars: Vec<char> = n.to_string().chars().collect();
+            chars.sort_unstable();
+            chars.into_iter().collect()
+        }
+
+        let power_of_two_set = get_power_of_two_sorted_set();
+        let sorted_n = int_to_sorted_str(n);
+
+        power_of_two_set.contains(&sorted_n)
+    }
+}
