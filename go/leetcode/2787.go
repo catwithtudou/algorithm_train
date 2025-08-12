acf74b78162fbb40fca53d57c390d1f36e9f83f9
catwithtudou <949812478@qq.com>
2025-08-12 09:53:31 +0800

    update: done the 2787

---
+++ go/leetcode/2787.go
@@ -0,0 +1,21 @@
+﻿package leetcode
+
+import "math"
+
+func numberOfWays(n int, x int) int {
+	dp := make([]int, n+1)
+	dp[0] = 1
+
+	for i := 1; powByMath(i, x) <= n; i++ {
+		v := powByMath(i, x)
+		for j := n; j >= v; j-- {
+			dp[j] += dp[j-v]
+		}
+	}
+
+	return dp[n] % (1e9 + 7)
+}
+
+func powByMath(i, x int) int {
+	return int(math.Pow(float64(i), float64(x)))
+}
--- rust/src/leetcode.rs
+++ rust/src/leetcode.rs
@@ -337,6 +337,7 @@ pub mod two_thousand_seven_hundred_sixty_five;
 pub mod two_thousand_seven_hundred_sixty_six;
 pub mod two_thousand_seven_hundred_seventy_nine;
 pub mod two_thousand_seven_hundred_eighty_six;
+pub mod two_thousand_seven_hundred_eighty_seven;
 pub mod two_thousand_seven_hundred_ninety_eight;
 pub mod two_thousand_seven_hundred_ninety_nine;
 pub mod two_thousand_eight_hundred_and_six;
