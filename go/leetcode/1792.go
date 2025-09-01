5b225fe9e9525fab317aaee8d6e2f633ec7be826
catwithtudou <949812478@qq.com>
2025-09-01 22:20:34 +0800

    update: done the 1792

---
+++ go/leetcode/1792.go
@@ -0,0 +1,43 @@
+﻿package leetcode
+
+import "container/heap"
+
+func maxAverageRatio(classes [][]int, extraStudents int) float64 {
+
+	n := len(classes)
+	h := make(ratioHp, n)
+
+	for i, c := range classes {
+		a, b := c[0], c[1]
+		h[i] = ratioTuple{float64(b-a) / float64(b*(b+1)), a, b}
+	}
+	heap.Init(&h)
+
+	for range extraStudents {
+		h[0].a++
+		h[0].b++
+		a, b := h[0].a, h[0].b
+		h[0].gain = float64(b-a) / float64(b*(b+1))
+		heap.Fix(&h, 0)
+	}
+
+	sum := 0.0
+
+	for _, t := range h {
+		sum += float64(t.a) / float64(t.b)
+	}
+	return sum / float64(n)
+}
+
+type ratioTuple struct {
+	gain float64
+	a, b int
+}
+
+type ratioHp []ratioTuple
+
+func (h ratioHp) Len() int           { return len(h) }
+func (h ratioHp) Less(i, j int) bool { return h[i].gain > h[j].gain }
+func (h ratioHp) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h ratioHp) Push(any)           {}
+func (h ratioHp) Pop() (_ any)       { return }
---
+++ rust/src/leetcode/one_thousand_seven_hundred_ninty_two.rs
@@ -0,0 +1,55 @@
+﻿use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+
+pub struct Solution;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct ClassInfo {
+    gain: f64,
+    pass: i32,
+    total: i32,
+}
+
+impl Eq for ClassInfo {}
+
+impl PartialOrd for ClassInfo {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.gain.partial_cmp(&other.gain)
+    }
+}
+
+impl Ord for ClassInfo {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).unwrap_or(Ordering::Equal)
+    }
+}
+
+impl Solution {
+    pub fn max_average_ratio(classes: Vec<Vec<i32>>, extra_students: i32) -> f64 {
+        let n = classes.len();
+        let mut h: BinaryHeap<ClassInfo> = classes
+            .into_iter()
+            .map(|c| {
+                let pass = c[0];
+                let total = c[1];
+                let gain = (total - pass) as f64 / (total as f64 * (total + 1) as f64);
+                ClassInfo { gain, pass, total }
+            })
+            .collect();
+
+        for _ in 0..extra_students {
+            if let Some(mut top) = h.peek_mut() {
+                top.pass += 1;
+                top.total += 1;
+                let pass = top.pass;
+                let total = top.total;
+                top.gain = (total - pass) as f64 / (total as f64 * (total + 1) as f64);
+            }
+        }
+
+        h.into_iter()
+            .map(|info| info.pass as f64 / info.total as f64)
+            .sum::<f64>()
+            / n as f64
+    }
+}
