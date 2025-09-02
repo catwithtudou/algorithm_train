64ede32f103f417459d04c75b599e174d3e5c36d
catwithtudou <949812478@qq.com>
2025-09-02 08:39:18 +0800

    update: done the 3025

---
+++ go/leetcode/3025.go
@@ -0,0 +1,28 @@
+﻿package leetcode
+
+import (
+	"math"
+	"slices"
+)
+
+func numberOfPairs3025(points [][]int) (ans int) {
+	slices.SortFunc(points, func(a, b []int) int {
+		if a[0] != b[0] {
+			return a[0] - b[0]
+		}
+		return b[1] - a[1]
+	})
+	for i, p := range points {
+		y1 := p[1]
+		maxY := math.MinInt
+		for _, q := range points[i+1:] {
+			y2 := q[1]
+			if y2 <= y1 && y2 > maxY {
+				maxY = y2
+				ans++
+			}
+		}
+	}
+
+	return
+}
---
+++ rust/src/leetcode/three_thousand_and_twenty_five.rs
@@ -0,0 +1,26 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn number_of_pairs(mut points: Vec<Vec<i32>>) -> i32 {
+        points.sort_unstable_by(|a, b| {
+            if a[0] != b[0] {
+                a[0].cmp(&b[0])
+            } else {
+                b[1].cmp(&a[1])
+            }
+        });
+        let mut ans = 0;
+        for i in 0..points.len() {
+            let p = &points[i];
+            let mut max_y = i32::MIN;
+            for j in i + 1..points.len() {
+                let q = &points[j];
+                if q[1] <= p[1] && q[1] > max_y {
+                    max_y = q[1];
+                    ans += 1;
+                }
+            }
+        }
+        ans
+    }
+}
