82f576f40769b4fb3cbd49790852cd1b0caafe6e
catwithtudou <949812478@qq.com>
2025-09-03 21:48:41 +0800

    update: done the 3027

---
+++ go/leetcode/3027.go
@@ -0,0 +1,28 @@
+﻿package leetcode
+
+import (
+	"cmp"
+	"math"
+	"slices"
+)
+
+func numberOfPairsII(points [][]int) (ans int) {
+	slices.SortFunc(points, func(a, b []int) int {
+		return cmp.Or(a[0]-b[0], b[1]-a[1])
+	})
+	for i, point := range points {
+		y1 := point[1]
+		maxY := math.MinInt
+		for _, p := range points[i+1:] {
+			y2 := p[1]
+			if y2 <= y1 && y2 > maxY {
+				maxY = y2
+				ans++
+			}
+			if maxY == y1 {
+				break
+			}
+		}
+	}
+	return
+}
---
+++ rust/src/leetcode/three_thousand_and_twenty_seven.rs
@@ -0,0 +1,26 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn number_of_pairs(mut points: Vec<Vec<i32>>) -> i32 {
+        let mut ans = 0;
+
+        points.sort_unstable_by_key(|p| (p[0], -p[1]));
+
+        for (i, p) in points.iter().enumerate() {
+            let mut max_y = i32::MIN;
+            let y1 = p[1];
+            for q in &points[i + 1..] {
+                let y2 = q[1];
+                if y2 <= y1 && y2 > max_y {
+                    max_y = y2;
+                    ans += 1;
+                }
+                if max_y == y1 {
+                    break;
+                }
+            }
+        }
+
+        ans
+    }
+}
