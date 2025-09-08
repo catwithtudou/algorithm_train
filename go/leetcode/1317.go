57cceccccc2542046f5ce36b4fdb30b2b29cc1be
catwithtudou <949812478@qq.com>
2025-09-08 22:08:53 +0800

    update: done the 1377

---
+++ go/leetcode/1317.go
@@ -0,0 +1,15 @@
+﻿package leetcode
+
+import (
+	"strconv"
+	"strings"
+)
+
+func getNoZeroIntegers(n int) []int {
+	for a := 1; ; a++ {
+		if !strings.ContainsRune(strconv.Itoa(a), '0') &&
+			!strings.ContainsRune(strconv.Itoa(n-a), '0') {
+			return []int{a, n - a}
+		}
+	}
+}
---
+++ rust/src/leetcode/one_thousand_three_hundred_and_seventeen.rs
@@ -0,0 +1,15 @@
+﻿pub struct Solution;
+
+use rand::Rng;
+
+impl Solution {
+    pub fn get_no_zero_integers(n: i32) -> Vec<i32> {
+        let mut rng = rand::thread_rng();
+        loop {
+            let a = rng.gen_range(1..n);
+            if !a.to_string().contains('0') && !(n - a).to_string().contains('0') {
+                return vec![a, n - a];
+            }
+        }
+    }
+}
