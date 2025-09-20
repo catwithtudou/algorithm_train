d50b7ff24a92b8ed55c664ea6b63c6cccf993112
catwithtudou <949812478@qq.com>
2025-09-20 17:29:51 +0800

    update: done the 3508

---
+++ go/leetcode/3508.go
@@ -0,0 +1,74 @@
+﻿package leetcode
+
+import "sort"
+
+type Router struct {
+	memoryLimit      int
+	data             []RouterData
+	dataSet          map[RouterData]struct{}
+	destToTimestamps map[int][]int
+}
+
+type RouterData struct {
+	source      int
+	destination int
+	timestamp   int
+}
+
+func RouterConstructor(memoryLimit int) Router {
+	return Router{
+		memoryLimit:      memoryLimit,
+		data:             make([]RouterData, 0),
+		dataSet:          make(map[RouterData]struct{}),
+		destToTimestamps: make(map[int][]int),
+	}
+}
+
+func (this *Router) AddPacket(source int, destination int, timestamp int) bool {
+	data := RouterData{
+		source:      source,
+		destination: destination,
+		timestamp:   timestamp,
+	}
+
+	if _, ok := this.dataSet[data]; ok {
+		return false
+	}
+
+	this.dataSet[data] = struct{}{}
+
+	if len(this.data) == this.memoryLimit {
+		this.ForwardPacket()
+	}
+
+	this.data = append(this.data, data)
+	this.destToTimestamps[destination] = append(this.destToTimestamps[destination], timestamp)
+
+	return true
+}
+
+func (this *Router) ForwardPacket() []int {
+	if len(this.data) == 0 {
+		return []int{}
+	}
+
+	data := this.data[0]
+	this.data = this.data[1:]
+	this.destToTimestamps[data.destination] = this.destToTimestamps[data.destination][1:]
+	delete(this.dataSet, data)
+
+	return []int{data.source, data.destination, data.timestamp}
+}
+
+func (this *Router) GetCount(destination int, startTime int, endTime int) int {
+	timestamps := this.destToTimestamps[destination]
+	return sort.SearchInts(timestamps, endTime+1) - sort.SearchInts(timestamps, startTime)
+}
+
+/**
+ * Your Router object will be instantiated and called as such:
+ * obj := Constructor(memoryLimit);
+ * param_1 := obj.AddPacket(source,destination,timestamp);
+ * param_2 := obj.ForwardPacket();
+ * param_3 := obj.GetCount(destination,startTime,endTime);
+ */
---
+++ rust/src/leetcode/three_thousand_five_hundred_and_eight.rs
@@ -0,0 +1,62 @@
+﻿use std::collections::{HashMap, HashSet, VecDeque};
+
+pub struct Solution;
+
+struct Router {
+    memory_limit: usize,
+    data_list: VecDeque<(i32, i32, i32)>,
+    data_set: HashSet<(i32, i32, i32)>,
+    dest_to_ts: HashMap<i32, (Vec<i32>, usize)>,
+}
+
+impl Router {
+    fn new(memory_limit: i32) -> Self {
+        Self {
+            memory_limit: memory_limit as usize,
+            data_list: VecDeque::new(),
+            data_set: HashSet::new(),
+            dest_to_ts: HashMap::new(),
+        }
+    }
+
+    fn add_packet(&mut self, source: i32, destination: i32, timestamp: i32) -> bool {
+        let data = (source, destination, timestamp);
+        if !self.data_set.insert(data) {
+            return false;
+        }
+
+        if self.data_list.len() == self.memory_limit {
+            self.forward_packet();
+        }
+
+        self.data_list.push_back(data);
+        self.dest_to_ts
+            .entry(destination)
+            .or_insert_with(|| (vec![], 0))
+            .0
+            .push(timestamp);
+
+        true
+    }
+
+    fn forward_packet(&mut self) -> Vec<i32> {
+        if let Some(data) = self.data_list.pop_front() {
+            self.data_set.remove(&data);
+            let (src, dest, ts) = data;
+            self.dest_to_ts.get_mut(&dest).unwrap().1 += 1;
+            vec![src, dest, ts]
+        } else {
+            vec![]
+        }
+    }
+
+    fn get_count(&self, destination: i32, start_time: i32, end_time: i32) -> i32 {
+        if let Some((ts, head)) = self.dest_to_ts.get(&destination) {
+            let left = ts[*head..].partition_point(|&x| x < start_time);
+            let right = ts[*head..].partition_point(|&x| x <= end_time);
+            (right - left) as _
+        } else {
+            0
+        }
+    }
+}
