b8a91b084ef39b00bfd96a795ddd8a0f71b71c81
catwithtudou <949812478@qq.com>
2025-09-25 21:48:54 +0800

    update: done the 120

---
+++ go/leetcode/120.go
@@ -0,0 +1,30 @@
+﻿package leetcode
+
+import "math"
+
+func minimumTotal(triangle [][]int) int {
+	n := len(triangle)
+	memo := make([][]int, n)
+	for i := range memo {
+		memo[i] = make([]int, n)
+		for j := range memo[i] {
+			memo[i][j] = math.MinInt
+		}
+	}
+
+	var dfs func(int, int) int
+
+	dfs = func(i, j int) int {
+		if i == n-1 {
+			return triangle[i][j]
+		}
+		p := &memo[i][j]
+		if *p != math.MinInt {
+			return *p
+		}
+		*p = min(dfs(i+1, j), dfs(i+1, j+1)) + triangle[i][j]
+		return *p
+	}
+
+	return dfs(0, 0)
+}
---
+++ rust/src/leetcode/one_hundred_and_twenty.rs
@@ -0,0 +1,20 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn minimum_total(triangle: Vec<Vec<i32>>) -> i32 {
+        fn dfs(i: usize, j: usize, triangle: &[Vec<i32>], memo: &mut [Vec<i32>]) -> i32 {
+            if i == triangle.len() - 1 {
+                return triangle[i][j];
+            }
+            if memo[i][j] != i32::MIN {
+                return memo[i][j];
+            }
+            memo[i][j] = dfs(i + 1, j, triangle, memo).min(dfs(i + 1, j + 1, triangle, memo))
+                + triangle[i][j];
+            memo[i][j]
+        }
+        let n = triangle.len();
+        let mut memo = vec![vec![i32::MIN; n]; n];
+        dfs(0, 0, &triangle, &mut memo)
+    }
+}
