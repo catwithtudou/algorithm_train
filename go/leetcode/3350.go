76fb631820a14d2665848af5668dabe6ef8837a5
catwithtudou <949812478@qq.com>
2025-10-15 17:04:17 +0800

    update: done the 3500

---
+++ go/leetcode/3350.go
@@ -0,0 +1,17 @@
+﻿package leetcode
+
+func maxIncreasingSubarrays(nums []int) (ans int) {
+	preCnt, cnt := 0, 0
+
+	for i, x := range nums {
+		cnt++
+		if i == len(nums)-1 || x >= nums[i+1] {
+			maxLen := max(min(preCnt, cnt), cnt/2)
+			ans = max(ans, maxLen)
+			preCnt = cnt
+			cnt = 0
+		}
+	}
+
+	return ans
+}
---
+++ rust/src/leetcode/three_thousand_three_hundred_and_fifty.rs
@@ -0,0 +1,17 @@
+﻿pub struct Solution;
+
+impl Solution {
+    pub fn max_increasing_subarrays(nums: Vec<i32>) -> i32 {
+        let (mut pre_cnt, mut cnt) = (0, 0);
+        let mut ans = 0;
+        for i in 0..nums.len() {
+            cnt += 1;
+            if i == nums.len() - 1 || nums[i] >= nums[i + 1] {
+                ans = ans.max((cnt / 2).max(cnt.min(pre_cnt)));
+                pre_cnt = cnt;
+                cnt = 0;
+            }
+        }
+        ans
+    }
+}
