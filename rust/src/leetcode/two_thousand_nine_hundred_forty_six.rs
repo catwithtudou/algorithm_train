1b9b1e51123dfbde7874e4a80c20448f3f6427d7
catwithtudou <949812478@qq.com>
2026-03-27 21:22:57 +0800

    update: update the 2946

--- rust/src/leetcode/two_thousand_nine_hundred_forty_six.rs
+++ rust/src/leetcode/two_thousand_nine_hundred_forty_six.rs
@@ -1,3 +1,16 @@
 pub struct Solution;
 
-// TODO: add implementation for problem 2946.
+impl Solution {
+    pub fn are_similar(mat: Vec<Vec<i32>>, k: i32) -> bool {
+        let n = mat[0].len();
+        let k = k  as usize;
+        for row in mat {
+            for j in 0..n {
+                if row[j] != row[(j+k) % n ] {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
