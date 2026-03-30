bac75b3f9c943bf202112d1eb90cbf4705c4a651
catwithtudou <949812478@qq.com>
2026-03-30 10:49:28 +0800

    update: update the 2840

---
+++ go/leetcode/2840.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func checkStrings(s1 string, s2 string) bool {
+	var cnt1, cnt2 [2][26]int
+
+	for i, v := range s1 {
+		cnt1[i%2][v-'a']++
+		cnt2[i%2][s2[i]-'a']++
+	}
+
+	return cnt1 == cnt2
+}
