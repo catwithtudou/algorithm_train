03f39da643eb774d53a2f013bebfa088015679e7
catwithtudou <949812478@qq.com>
2026-05-15 11:09:20 +0800

    update: update the 2784

--- go/leetcode/2784.go
+++ go/leetcode/2784.go
@@ -1,3 +1,14 @@
 package leetcode
 
-// TODO: add solution for problem 2784.
+func isGood(nums []int) bool {
+	n := len(nums) - 1
+	cnt := make([]int, n+1)
+	for _, x := range nums {
+		if x > n ||
+			x == n && cnt[x] > 1 || x < n && cnt[x] > 0 {
+			return false
+		}
+		cnt[x]++
+	}
+	return true
+}
